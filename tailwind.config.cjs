b9fea1a0ea953cbb472bbb1a0cc2d394dffd9dca
Matt Proctor <proctormatt0@gmail.com>
2023-03-22 18:28:29 -0700

    Added custom color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,20 @@ const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      primary: {
+        dark: "#292B4C",
+        med: "#3D3F71",
+        light: "#A3A5D7",
+      },
+      secondary: {
+        dark: "#138583",
+        med: "#00C6C5",
+        light: "#B3FFFF",
+      },
+    },
   },
   plugins: [],
 };
