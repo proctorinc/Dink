facbb1293535cfeb7418e2255637fa99dc1d4ea0
Matt Proctor <proctormatt0@gmail.com>
2023-03-22 18:27:30 -0700

    Added utils functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,30 @@
+import { Prisma } from "@prisma/client";
 import type { Decimal } from "@prisma/client/runtime";
 
-export const formatToCurrency = (amount: Decimal | null) => {
+export const formatToCurrency = (amount: Decimal | undefined | null) => {
   const USD = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   });
   return amount ? USD.format(Number(amount)) : "$0.00";
+};
+
+export const formatToTitleCase = (str: string) => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
+};
+
+export const formatToPercentage = (
+  numerator: Decimal | undefined | null,
+  divisor: Decimal | undefined | null
+) => {
+  if (numerator && divisor) {
+    const result = Prisma.Decimal.div(numerator, divisor);
+    console.log(result);
+    return `${result?.toFixed(0)}%`;
+  }
+  return "0%";
 };
