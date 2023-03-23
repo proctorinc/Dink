be905abb3a2eb027426a6e150a885757a51e12b7
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 07:49:24 -0700

    Moved Head component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +13,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Dink</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center bg-primary-dark p-2 text-white">
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
