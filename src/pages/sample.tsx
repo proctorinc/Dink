d64c6129ffe1396974dce67498830c1375cb0668
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 15:37:49 -0700

    Cleaned up project for deployment

--- src/pages/sample.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { api } from "@utils/api";
-
-const Test = () => {
-  const hello = api.example.hello.useQuery({ text: "from TRPC" });
-  return <div>{hello.data ? hello.data.greeting : "Loading..."}</div>;
-};
-
-export default Test;
