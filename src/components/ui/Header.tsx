39696835d4b74827b6f305e8b16f49e5108683b7
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 14:39:52 -0700

    Made icon optional in Header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import type { FC, ReactNode } from "react";
+import React, { type FC, type ReactNode } from "react";
 
 type HeaderProps = {
   title: ReactNode;
   subtitle: ReactNode;
-  icon: ReactNode;
+  icon?: ReactNode;
 };
 
 const Header: FC<HeaderProps> = ({ title, subtitle, icon }) => {
@@ -14,7 +13,9 @@ const Header: FC<HeaderProps> = ({ title, subtitle, icon }) => {
         <h1 className="text-4xl font-bold">{title}</h1>
         <h2 className="text-xl font-light text-primary-light">{subtitle}</h2>
       </div>
-      <div className="relative flex items-center justify-center">{icon}</div>
+      {icon && (
+        <div className="relative flex items-center justify-center">{icon}</div>
+      )}
     </div>
   );
 };
