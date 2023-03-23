10c831fac4123e6fd99b12e388c8f79a3cfe7b18
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 14:45:28 -0700

    Added user profile page

---
+++ src/pages/app/user/index.tsx
@@ -0,0 +1,25 @@
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Header from "~/components/ui/Header";
+
+const UserPage = () => {
+  const { data: sessionData } = useSession();
+  return (
+    <div className="container flex max-w-md flex-col items-center justify-center gap-12 p-4">
+      <div className="flex w-full flex-col items-center gap-4">
+        <Header title="Profile" subtitle={""} />
+        {/* <Image
+          width={100}
+          height={100}
+          src={sessionData?.user.image ?? "https://none"}
+          alt="user-image"
+        /> */}
+        {/* <span>{sessionData?.user.image}</span> */}
+        <span>{sessionData?.user.name}</span>
+        <span>{sessionData?.user.email}</span>
+      </div>
+    </div>
+  );
+};
+
+export default UserPage;
