cfd256a2593fec02e00f04e2509ccfb68312ba7b
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 07:48:50 -0700

    Refactored common code into header file

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import type { FC, ReactNode } from "react";
+
+type HeaderProps = {
+  title: ReactNode;
+  subtitle: ReactNode;
+  icon: ReactNode;
+};
+
+const Header: FC<HeaderProps> = ({ title, subtitle, icon }) => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div className="flex flex-col">
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <h2 className="text-xl font-light text-primary-light">{subtitle}</h2>
+      </div>
+      <div className="relative flex items-center justify-center">{icon}</div>
+    </div>
+  );
+};
+
+export default Header;
