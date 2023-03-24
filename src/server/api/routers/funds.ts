0372ce4793e4584fc915f1233afd1db60afca130
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 23:33:36 -0700

    Changed funds api to include funds total amount

--- src/server/api/routers/funds.ts
+++ src/server/api/routers/funds.ts
@@ -1,11 +1,25 @@
+import { type Fund, Prisma } from "@prisma/client";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
+function sumFundsBalance(funds: Fund[]) {
+  return funds.reduce((acc, fund) => {
+    return Prisma.Decimal.add(
+      acc,
+      fund.initial_amount ?? new Prisma.Decimal(0)
+    );
+  }, new Prisma.Decimal(0));
+}
+
 export const fundsRouter = createTRPCRouter({
-  getAll: protectedProcedure.query(({ ctx }) => {
-    return ctx.prisma.fund.findMany({
+  getAllData: protectedProcedure.query(async ({ ctx }) => {
+    const funds = await ctx.prisma.fund.findMany({
       where: {
         userId: ctx.session.user.id,
       },
     });
+    return {
+      amount: sumFundsBalance(funds),
+      funds: funds,
+    };
   }),
 });
