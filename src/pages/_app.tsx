729d9741db9822bd53567450a4b8ceecb9e08360
Matt Proctor <proctormatt0@gmail.com>
2023-03-23 23:32:43 -0700

    Added loader to page loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,14 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { useState } from "react";
 import Head from "next/head";
 import Router from "next/router";
-import { useState } from "react";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Navbar from "~/components/ui/Navbar";
 import MobileNavbar from "~/components/ui/MobileNavbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -34,7 +36,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
       </Head>
       <main className="flex flex-col items-center text-white">
         {loading ? (
-          <div>Loading...</div>
+          <div className="pt-64">
+            <FontAwesomeIcon
+              className="animate-spin text-primary-light"
+              size="2xl"
+              icon={faSpinner}
+            />
+          </div>
         ) : (
           <div className="container flex max-w-md flex-col items-center justify-center gap-12 px-4 pb-28 sm:pb-4">
             <div className="flex w-full flex-col items-center gap-4">
