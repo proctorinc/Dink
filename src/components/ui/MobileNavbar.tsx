3c1f2cc7808529b001e37e0e7fe1c74d0e498267
Matt Proctor <proctormatt0@gmail.com>
2023-03-25 14:25:00 -0700

    Refactored UI to loop over fixed routes

--- src/components/ui/MobileNavbar.tsx
+++ src/components/ui/MobileNavbar.tsx
@@ -11,45 +11,39 @@ import { useRouter } from "next/router";
 const MobileNavbar = () => {
   const router = useRouter();
 
+  const routes = [
+    { path: "/accounts", icon: faBuildingColumns },
+    { path: "/budget", icon: faCalendarAlt },
+    { path: "/", icon: faHome },
+    { path: "/funds", icon: faPiggyBank },
+    { path: "/transactions", icon: faReceipt },
+  ];
+
   return (
     <nav className="fixed inset-x-0 bottom-0 z-50 flex w-full justify-center text-primary-light sm:hidden">
       <div className="flex h-full w-full max-w-md flex-col rounded-t-3xl bg-primary-med/90 px-4 pt-2 pb-10 backdrop-blur-md">
         <div className="flex items-center justify-around">
-          <button className="h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
-            <FontAwesomeIcon
-              size="xl"
-              icon={faBuildingColumns}
-              onClick={() => void router.push("/accounts")}
-            />
-          </button>
-          <button className="h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
-            <FontAwesomeIcon
-              size="xl"
-              icon={faCalendarAlt}
-              onClick={() => void router.push("/budget")}
-            />
-          </button>
-          <button className="h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
-            <FontAwesomeIcon
-              size="xl"
-              icon={faHome}
-              onClick={() => void router.push("/")}
-            />
-          </button>
-          <button className="h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
-            <FontAwesomeIcon
-              size="xl"
-              icon={faPiggyBank}
-              onClick={() => void router.push("/funds")}
-            />
-          </button>
-          <button className="h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
-            <FontAwesomeIcon
-              size="xl"
-              icon={faReceipt}
-              onClick={() => void router.push("/transactions")}
-            />
-          </button>
+          {routes.map((route) => {
+            const activeStyle =
+              "h-10 w-10 rounded-full bg-primary-light text-primary-med";
+            const inactiveStyle =
+              "h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med";
+
+            return (
+              <button
+                key={route.path}
+                className={
+                  router.pathname === route.path ? activeStyle : inactiveStyle
+                }
+              >
+                <FontAwesomeIcon
+                  size="xl"
+                  icon={route.icon}
+                  onClick={() => void router.push(route.path)}
+                />
+              </button>
+            );
+          })}
         </div>
       </div>
     </nav>
