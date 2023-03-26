bcbc6344b3f8dfcdde3f2273619fb5bb6b5ac430
Matt Proctor <proctormatt0@gmail.com>
2023-03-25 17:28:51 -0700

    Reworked header component

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,20 +2,18 @@ import React, { type FC, type ReactNode } from "react";
 
 type HeaderProps = {
   title: ReactNode;
-  subtitle: ReactNode;
-  icon?: ReactNode;
+  subtitle?: ReactNode;
 };
 
-const Header: FC<HeaderProps> = ({ title, subtitle, icon }) => {
+const Header: FC<HeaderProps> = ({ title, subtitle }) => {
   return (
     <div className="flex w-full items-center justify-between">
       <div className="flex flex-col">
         <h1 className="text-4xl font-bold">{title}</h1>
-        <h2 className="text-2xl font-light text-primary-light">{subtitle}</h2>
+        {subtitle && (
+          <h2 className="text-2xl font-light text-primary-light">{subtitle}</h2>
+        )}
       </div>
-      {icon && (
-        <div className="relative flex items-center justify-center">{icon}</div>
-      )}
     </div>
   );
 };
