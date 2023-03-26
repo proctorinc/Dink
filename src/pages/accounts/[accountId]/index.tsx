d120f9497a414fc85bd4247a479da4372bf32d36
Matt Proctor <proctormatt0@gmail.com>
2023-03-25 17:30:26 -0700

    Restyled header and account info

--- src/pages/accounts/[accountId]/index.tsx
+++ src/pages/accounts/[accountId]/index.tsx
@@ -1,9 +1,9 @@
-import { faArrowLeft, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Header from "~/components/ui/Header";
-import { formatToCurrency } from "~/utils";
+import BackHeader from "~/components/ui/BackHeader";
+import { formatToCurrency, formatToTitleCase } from "~/utils";
 import { api } from "~/utils/api";
 
 const AccountPage = () => {
@@ -21,20 +21,17 @@ const AccountPage = () => {
 
   return (
     <>
-      <div className="w-full">
-        <FontAwesomeIcon
-          icon={faArrowLeft}
-          size="xl"
-          onClick={() => void router.back()}
-        />
-      </div>
-      <Header
+      <BackHeader
         title={accountData?.data?.name}
         subtitle={formatToCurrency(accountData?.data?.current)}
       />
-      <span>
-        {accountData?.data?.official_name} {accountData?.data?.mask} |{" "}
-        {accountData?.data?.type} : {accountData?.data?.subtype}
+      <span className="flex h-fit items-center justify-center rounded-lg bg-primary-med px-2 py-1 text-xs font-bold text-primary-light">
+        {accountData?.data?.official_name}
+      </span>
+      <span className="text-md text-primary-light">
+        {formatToTitleCase(accountData?.data?.type)} /{" "}
+        {formatToTitleCase(accountData?.data?.subtype)} /{" "}
+        {accountData?.data?.mask}
       </span>
       <button
         className="flex h-fit items-center gap-2 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light"
@@ -51,20 +48,43 @@ const AccountPage = () => {
         <FontAwesomeIcon className="h-4 w-4" icon={faPlus} />
         <span>Add Demo Transaction</span>
       </button>
-      <h2 className="text-xl text-primary-light">Transactions</h2>
-      {accountData.data &&
-        accountData.data.transactions.map((transaction) => {
-          return (
-            <div
-              key={transaction.id}
-              className="group flex w-full cursor-pointer items-center justify-between rounded-xl bg-primary-med p-4 hover:bg-primary-light hover:text-primary-dark"
-            >
-              <span>{transaction.name}</span>
-              <span>{formatToCurrency(transaction.amount)}</span>
-              <span>{transaction.date.toLocaleDateString()}</span>
-            </div>
-          );
-        })}
+      <div className="w-full">
+        <h2 className="text-left text-xl text-primary-light">Transactions</h2>
+      </div>
+      {!accountData?.data?.transactions.length && (
+        <div className="group flex w-full items-center justify-between rounded-xl bg-primary-med px-4 py-2">
+          None
+        </div>
+      )}
+      <div className="flex w-full flex-col gap-3">
+        {accountData.data &&
+          accountData.data.transactions.map((transaction) => {
+            return (
+              <div
+                key={transaction.id}
+                className="group flex w-full cursor-pointer items-center justify-between rounded-xl bg-primary-med px-4 py-2 hover:bg-primary-light hover:text-primary-dark"
+              >
+                <div className="flex flex-col gap-1">
+                  <span className="text-lg font-bold">{transaction.name}</span>
+                  <span className="text-xs text-primary-light group-hover:text-primary-med">
+                    {"Uncategorized"}
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1 text-right">
+                  <span className="text-lg font-bold text-primary-light group-hover:text-primary-med">
+                    {formatToCurrency(transaction.amount)}
+                  </span>
+                  <span className="text-xs text-primary-light group-hover:text-primary-med">
+                    {transaction.date.toLocaleString("en-us", {
+                      month: "short",
+                      day: "numeric",
+                    })}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
