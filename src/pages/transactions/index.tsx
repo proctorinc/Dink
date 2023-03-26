79b6eb755a433aee3cb37ca9bba10e22dd524a78
Matt Proctor <proctormatt0@gmail.com>
2023-03-25 17:30:54 -0700

    Reworked transactions UI

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,3 +1,5 @@
+import { faTags } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Header from "~/components/ui/Header";
 import MonthYearSelector from "~/components/ui/MonthYearSelector";
 import { formatToCurrency } from "~/utils";
@@ -8,41 +10,43 @@ const TransactionsPage = () => {
 
   return (
     <>
-      <Header title="Transactions" subtitle="???" />
-      <div className="flex w-full items-center gap-2">
-        <button
-          disabled
-          className="flex h-fit items-center gap-2 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark group-hover:text-secondary-light"
-        >
-          <span>All</span>
-        </button>
-        <button
-          disabled
-          className="flex h-fit items-center gap-2 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark group-hover:text-secondary-light"
-        >
-          <span>Uncategorized</span>
-        </button>
-        <button
-          disabled
-          className="flex h-fit items-center gap-2 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark group-hover:text-secondary-light"
-        >
-          <span>Categorized</span>
+      <Header title="Transactions" />
+      <div className="flex w-full gap-2">
+        <button className="flex h-fit items-center gap-1 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light">
+          <FontAwesomeIcon size="sm" icon={faTags} />
+          <span>Categorize</span>
         </button>
       </div>
       <MonthYearSelector />
-      {transactionData.data &&
-        transactionData.data.map((transaction) => {
-          return (
-            <div
-              key={transaction.id}
-              className="group flex w-full cursor-pointer items-center justify-between rounded-xl bg-primary-med p-4 hover:bg-primary-light hover:text-primary-dark"
-            >
-              <span>{transaction.name}</span>
-              <span>{formatToCurrency(transaction.amount)}</span>
-              <span>{transaction.date.toLocaleDateString()}</span>
-            </div>
-          );
-        })}
+      <div className="flex flex-col gap-3">
+        {transactionData.data &&
+          transactionData.data.map((transaction) => {
+            return (
+              <div
+                key={transaction.id}
+                className="group flex w-full cursor-pointer items-center justify-between rounded-xl bg-primary-med px-4 py-2 hover:bg-primary-light hover:text-primary-dark"
+              >
+                <div className="flex flex-col gap-1">
+                  <span className="text-lg font-bold">{transaction.name}</span>
+                  <span className="text-xs text-primary-light group-hover:text-primary-med">
+                    {"Uncategorized"}
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1 text-right">
+                  <span className="text-lg font-bold text-primary-light group-hover:text-primary-med">
+                    {formatToCurrency(transaction.amount)}
+                  </span>
+                  <span className="text-xs text-primary-light group-hover:text-primary-med">
+                    {transaction.date.toLocaleString("en-us", {
+                      month: "short",
+                      day: "numeric",
+                    })}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
