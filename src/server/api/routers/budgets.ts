a411904a5ceb9d1462bdca2a4dacd293283d8800
Matt Proctor <proctormatt0@gmail.com>
2023-03-27 12:02:40 -0700

    Added budget transaction sum

--- src/server/api/routers/budgets.ts
+++ src/server/api/routers/budgets.ts
@@ -1,9 +1,27 @@
-import { Prisma, type Budget } from "@prisma/client";
+import { Prisma, type Transaction, type Budget } from "@prisma/client";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 function sumBudgetGoals(budgets: Budget[]) {
   return budgets.reduce((acc, budget) => {
     return Prisma.Decimal.add(acc, budget.goal ?? new Prisma.Decimal(0));
+  }, new Prisma.Decimal(0));
+}
+
+function sumBudgetTransactions(
+  budgets: (Budget & {
+    source_transactions: Transaction[];
+  })[]
+) {
+  return budgets.reduce((acc, budget) => {
+    return Prisma.Decimal.add(
+      acc,
+      budget.source_transactions.reduce((acc, transaction) => {
+        return Prisma.Decimal.add(
+          acc,
+          transaction.amount ?? new Prisma.Decimal(0)
+        );
+      }, new Prisma.Decimal(0))
+    );
   }, new Prisma.Decimal(0));
 }
 
@@ -13,9 +31,12 @@ export const budgetsRouter = createTRPCRouter({
       where: {
         userId: ctx.session.user.id,
       },
+      include: {
+        source_transactions: true,
+      },
     });
     const goal = sumBudgetGoals(budgets);
-    const spent = new Prisma.Decimal(0);
+    const spent = sumBudgetTransactions(budgets);
 
     return {
       goal: goal,
