498443bceca0e727023209c81330d5cc7b3a25af
Matt Proctor <proctormatt0@gmail.com>
2023-03-27 12:00:12 -0700

    Removed in between page loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,29 +5,15 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { useState } from "react";
 import Head from "next/head";
-import Router from "next/router";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Navbar from "~/components/ui/Navbar";
 import MobileNavbar from "~/components/ui/MobileNavbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  const [loading, setLoading] = useState(false);
-
-  // Router.events.on("routeChangeStart", () => {
-  //   setLoading(true);
-  // });
-
-  // Router.events.on("routeChangeComplete", () => {
-  //   setLoading(false);
-  // });
-
   return (
     <SessionProvider session={session}>
       <Navbar />
@@ -35,21 +21,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <title>Dink</title>
       </Head>
       <main className="flex flex-col items-center text-white">
-        {loading ? (
-          <div className="pt-64">
-            <FontAwesomeIcon
-              className="animate-spin text-primary-light"
-              size="2xl"
-              icon={faSpinner}
-            />
+        <div className="container flex max-w-md flex-col items-center justify-center gap-12 px-4 pb-28 pt-5 sm:pb-4">
+          <div className="flex w-full flex-col items-center gap-4">
+            <Component {...pageProps} />
           </div>
-        ) : (
-          <div className="container flex max-w-md flex-col items-center justify-center gap-12 px-4 pb-28 pt-5 sm:pb-4">
-            <div className="flex w-full flex-col items-center gap-4">
-              <Component {...pageProps} />
-            </div>
-          </div>
-        )}
+        </div>
       </main>
       <MobileNavbar />
     </SessionProvider>
