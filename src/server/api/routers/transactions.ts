18524e8e692eb0cea18160739ebc99680790cc89
Matt Proctor <proctormatt0@gmail.com>
2023-03-26 23:43:51 -0700

    Made uncategorized query and categorize mutation

--- src/server/api/routers/transactions.ts
+++ src/server/api/routers/transactions.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const transactionsRouter = createTRPCRouter({
@@ -6,6 +7,50 @@ export const transactionsRouter = createTRPCRouter({
       where: {
         userId: ctx.session.user.id,
       },
+      include: {
+        budgetSource: true,
+        fundSource: true,
+      },
     });
   }),
+  getUncategorized: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.prisma.transaction.findMany({
+      where: {
+        userId: ctx.session.user.id,
+        sourceType: null,
+      },
+    });
+  }),
+  categorizeTransaction: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().nullable(),
+        type: z.string().nullable(),
+        sourceId: z.string(),
+      })
+    )
+    .mutation(({ input, ctx }) => {
+      if (input.type === "fund") {
+        return ctx.prisma.transaction.updateMany({
+          where: {
+            userId: ctx.session.user.id,
+            id: input.id ?? "",
+          },
+          data: {
+            sourceType: input.type,
+            fundSourceId: input.sourceId,
+          },
+        });
+      }
+      return ctx.prisma.transaction.updateMany({
+        where: {
+          userId: ctx.session.user.id,
+          id: input.id ?? "",
+        },
+        data: {
+          sourceType: input.type,
+          budgetSourceId: input.sourceId,
+        },
+      });
+    }),
 });
