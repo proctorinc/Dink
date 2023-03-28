62d906bcb79cf7a73b592fe34ffca659cdb3be5d
Matt Proctor <proctormatt0@gmail.com>
2023-03-28 16:34:09 -0700

    Added back prop to header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,18 +1,35 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 import React, { type FC, type ReactNode } from "react";
 
 type HeaderProps = {
   title: ReactNode;
   subtitle?: ReactNode;
+  back?: boolean;
 };
 
-const Header: FC<HeaderProps> = ({ title, subtitle }) => {
+const Header: FC<HeaderProps> = ({ title, subtitle, back }) => {
+  const router = useRouter();
+
   return (
     <div className="flex w-full items-center justify-between">
-      <div className="flex flex-col">
-        <h1 className="text-4xl font-bold">{title}</h1>
-        {subtitle && (
-          <h2 className="text-2xl font-light text-primary-light">{subtitle}</h2>
+      <div className="flex items-center gap-4">
+        {back && (
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            size="xl"
+            onClick={() => void router.back()}
+          />
         )}
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-bold">{title}</h1>
+          {subtitle && (
+            <h2 className="text-2xl font-light text-primary-light">
+              {subtitle}
+            </h2>
+          )}
+        </div>
       </div>
     </div>
   );
