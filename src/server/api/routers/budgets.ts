73a136f22cbf07f7ac5a0053495dd237121afbb1
Matt Proctor <proctormatt0@gmail.com>
2023-03-28 13:55:32 -0700

    Added functions to sum budget and fund transactions and return amounts in data

--- src/server/api/routers/budgets.ts
+++ src/server/api/routers/budgets.ts
@@ -1,5 +1,35 @@
-import { Prisma, type Transaction, type Budget } from "@prisma/client";
+import { Prisma, type Budget } from "@prisma/client";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+type TransactionAmount = {
+  amount: Prisma.Decimal;
+};
+
+type BudgetAmount = {
+  spent: Prisma.Decimal;
+};
+
+function sumTransactions(transactions: TransactionAmount[]) {
+  return transactions.reduce((acc, transaction) => {
+    return Prisma.Decimal.add(acc, transaction.amount);
+  }, new Prisma.Decimal(0));
+}
+
+function sumBudgetTransactions(
+  budgets: (Budget & {
+    source_transactions: TransactionAmount[];
+  })[]
+) {
+  return budgets.map((budget) => {
+    const { source_transactions, ...otherFields } = budget;
+    const spent = sumTransactions(source_transactions);
+    return {
+      ...otherFields,
+      spent,
+      leftover: Prisma.Decimal.sub(budget.goal, spent),
+    };
+  });
+}
 
 function sumBudgetGoals(budgets: Budget[]) {
   return budgets.reduce((acc, budget) => {
@@ -7,21 +37,9 @@ function sumBudgetGoals(budgets: Budget[]) {
   }, new Prisma.Decimal(0));
 }
 
-function sumBudgetTransactions(
-  budgets: (Budget & {
-    source_transactions: Transaction[];
-  })[]
-) {
+function sumTotalBudgetSpent(budgets: (Budget & BudgetAmount)[]) {
   return budgets.reduce((acc, budget) => {
-    return Prisma.Decimal.add(
-      acc,
-      budget.source_transactions.reduce((acc, transaction) => {
-        return Prisma.Decimal.add(
-          acc,
-          transaction.amount ?? new Prisma.Decimal(0)
-        );
-      }, new Prisma.Decimal(0))
-    );
+    return Prisma.Decimal.add(acc, budget.spent);
   }, new Prisma.Decimal(0));
 }
 
@@ -32,17 +50,22 @@ export const budgetsRouter = createTRPCRouter({
         userId: ctx.session.user.id,
       },
       include: {
-        source_transactions: true,
+        source_transactions: {
+          select: {
+            amount: true,
+          },
+        },
       },
     });
+    const budgetsWithAmounts = sumBudgetTransactions(budgets);
+    const spent = sumTotalBudgetSpent(budgetsWithAmounts);
     const goal = sumBudgetGoals(budgets);
-    const spent = sumBudgetTransactions(budgets);
 
     return {
       goal: goal,
       spent: spent,
       leftover: Prisma.Decimal.sub(goal, spent),
-      budgets: budgets,
+      budgets: budgetsWithAmounts,
     };
   }),
 });
--- src/server/api/routers/funds.ts
+++ src/server/api/routers/funds.ts
@@ -1,13 +1,38 @@
 import { type Fund, Prisma } from "@prisma/client";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
-function sumFundsBalance(funds: Fund[]) {
-  return funds.reduce((acc, fund) => {
-    return Prisma.Decimal.add(
-      acc,
-      fund.initial_amount ?? new Prisma.Decimal(0)
-    );
+type TransactionAmount = {
+  amount: Prisma.Decimal;
+};
+
+type FundAmount = {
+  amount: Prisma.Decimal;
+};
+
+function sumTransactions(transactions: TransactionAmount[]) {
+  return transactions.reduce((acc, transaction) => {
+    return Prisma.Decimal.add(acc, transaction.amount);
   }, new Prisma.Decimal(0));
+}
+
+function sumTotalFundAmount(funds: (Fund & FundAmount)[]) {
+  return funds.reduce((acc, fund) => {
+    return Prisma.Decimal.add(acc, fund.amount);
+  }, new Prisma.Decimal(0));
+}
+
+function sumFundTransactions(
+  funds: (Fund & {
+    source_transactions: TransactionAmount[];
+  })[]
+) {
+  return funds.map((fund) => {
+    const { source_transactions, ...otherFields } = fund;
+    return {
+      ...otherFields,
+      amount: sumTransactions(source_transactions),
+    };
+  });
 }
 
 export const fundsRouter = createTRPCRouter({
@@ -16,10 +41,21 @@ export const fundsRouter = createTRPCRouter({
       where: {
         userId: ctx.session.user.id,
       },
+      include: {
+        source_transactions: {
+          select: {
+            amount: true,
+          },
+        },
+      },
     });
+    const fundsWithAmounts = sumFundTransactions(funds);
+
+    console.log(sumTotalFundAmount(fundsWithAmounts));
+
     return {
-      amount: sumFundsBalance(funds),
-      funds: funds,
+      total: sumTotalFundAmount(fundsWithAmounts),
+      funds: fundsWithAmounts,
     };
   }),
 });
