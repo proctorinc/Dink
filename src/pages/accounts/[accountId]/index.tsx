69acea1879e3024a95f404f884fa609ecc6ad8e5
Matt Proctor <proctormatt0@gmail.com>
2023-03-28 13:53:56 -0700

    Added transaction component

--- src/pages/accounts/[accountId]/index.tsx
+++ src/pages/accounts/[accountId]/index.tsx
@@ -2,6 +2,7 @@ import { faPlus, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Transaction from "~/components/transactions/Transaction";
 import BackHeader from "~/components/ui/BackHeader";
 import { formatToCurrency, formatToTitleCase } from "~/utils";
 import { api } from "~/utils/api";
@@ -70,38 +71,9 @@ const AccountPage = () => {
       )}
       <div className="flex w-full flex-col gap-3">
         {accountData.data &&
-          accountData.data.transactions.map((transaction) => {
-            return (
-              <div
-                key={transaction.id}
-                className="group flex w-full cursor-pointer items-center justify-between rounded-xl bg-primary-med px-4 py-2 hover:bg-primary-light hover:text-primary-dark"
-              >
-                <div className="flex flex-col gap-1">
-                  <span className="text-lg font-bold">{transaction.name}</span>
-                  <span className="text-xs text-primary-light group-hover:text-primary-med">
-                    {transaction.sourceType
-                      ? transaction.sourceType
-                      : "Uncategorized"}
-                    {transaction.sourceType === "fund" &&
-                      ` / ${transaction.fundSource?.name ?? ""}`}
-                    {transaction.sourceType === "budget" &&
-                      ` / ${transaction.budgetSource?.name ?? ""}`}
-                  </span>
-                </div>
-                <div className="flex flex-col gap-1 text-right">
-                  <span className="text-lg font-bold text-primary-light group-hover:text-primary-med">
-                    {formatToCurrency(transaction.amount)}
-                  </span>
-                  <span className="text-xs text-primary-light group-hover:text-primary-med">
-                    {transaction.date.toLocaleString("en-us", {
-                      month: "short",
-                      day: "numeric",
-                    })}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+          accountData.data.transactions.map((transaction) => (
+            <Transaction key={transaction.id} data={transaction} />
+          ))}
       </div>
     </>
   );
--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -5,9 +5,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
+import Transaction from "~/components/transactions/Transaction";
 import Header from "~/components/ui/Header";
 import MonthYearSelector from "~/components/ui/MonthYearSelector";
-import { formatToCurrency } from "~/utils";
 import { api } from "~/utils/api";
 
 const TransactionsPage = () => {
@@ -17,10 +17,6 @@ const TransactionsPage = () => {
   return (
     <>
       <Header title="Transactions" />
-      {/* <div className="flex gap-2">
-        <div className="h-32 w-32 rounded-xl bg-primary-med p-4">20</div>
-        <div className="h-32 w-32 rounded-xl bg-primary-med p-4">50%</div>
-      </div> */}
       <div className="flex w-full gap-2">
         <button className="flex h-fit items-center gap-1 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light">
           <FontAwesomeIcon size="sm" icon={faMagnifyingGlass} />
@@ -48,38 +44,9 @@ const TransactionsPage = () => {
           </div>
         )}
         {transactionData.data &&
-          transactionData.data.map((transaction) => {
-            return (
-              <div
-                key={transaction.id}
-                className="group flex w-full cursor-pointer items-center justify-between rounded-xl bg-primary-med px-4 py-2 hover:bg-primary-light hover:text-primary-dark"
-              >
-                <div className="flex flex-col gap-1">
-                  <span className="text-lg font-bold">{transaction.name}</span>
-                  <span className="text-xs text-primary-light group-hover:text-primary-med">
-                    {transaction.sourceType
-                      ? transaction.sourceType
-                      : "Uncategorized"}
-                    {transaction.sourceType === "fund" &&
-                      ` / ${transaction.fundSource?.name ?? ""}`}
-                    {transaction.sourceType === "budget" &&
-                      ` / ${transaction.budgetSource?.name ?? ""}`}
-                  </span>
-                </div>
-                <div className="flex flex-col gap-1 text-right">
-                  <span className="text-lg font-bold text-primary-light group-hover:text-primary-med">
-                    {formatToCurrency(transaction.amount)}
-                  </span>
-                  <span className="text-xs text-primary-light group-hover:text-primary-med">
-                    {transaction.date.toLocaleString("en-us", {
-                      month: "short",
-                      day: "numeric",
-                    })}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+          transactionData.data.map((transaction) => (
+            <Transaction key={transaction.id} data={transaction} />
+          ))}
       </div>
     </>
   );
