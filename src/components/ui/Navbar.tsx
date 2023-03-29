38186e0fc6e34f7e15678a43ce85bb690a5ccded
Matt Proctor <proctormatt0@gmail.com>
2023-03-29 16:57:53 -0700

    Made UI changes to navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <nav className="absolute sticky top-0 z-50 flex w-full justify-center bg-primary-dark/90 py-2 pl-2 pr-4 text-primary-light backdrop-blur-sm md:flex">
-      <div className="flex w-full max-w-lg items-center justify-center justify-between">
+      <div className="flex w-full max-w-lg items-center justify-center justify-between sm:max-w-4xl">
         <button
           className="flex items-center gap-2 rounded-xl py-1 px-3 text-3xl font-bold"
           onClick={() => void router.push("/")}
@@ -26,12 +26,12 @@ const Navbar = () => {
         </button>
         {sessionData?.user && (
           <button
-            className="h-8 w-8 rounded-full bg-secondary-dark text-secondary-med hover:bg-secondary-med hover:text-secondary-light"
+            className="aspect-square w-10 rounded-full bg-secondary-dark text-secondary-med hover:bg-secondary-med hover:text-secondary-light"
             onClick={() => void router.push("/profile")}
           >
             {sessionData?.user.image ? (
               <Image
-                className="w-8 rounded-full border-2 border-primary-med"
+                className="w-full rounded-full border-2 border-primary-med"
                 width={10}
                 height={10}
                 src={sessionData?.user.image ?? "/favicon.ico"}
