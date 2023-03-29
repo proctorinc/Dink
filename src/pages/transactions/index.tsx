0292be210cc8489b389ceb58eae84487c400c9d8
Matt Proctor <proctormatt0@gmail.com>
2023-03-29 12:50:07 -0700

    Added monthly context for transactions

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -5,25 +5,30 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
+import { useMonthContext } from "~/components/hooks/useMonthContext";
 import Transaction from "~/components/transactions/Transaction";
 import Header from "~/components/ui/Header";
-import MonthYearSelector from "~/components/ui/MonthYearSelector";
+import MonthYearSelector from "~/components/ui/MonthSelector";
 import { api } from "~/utils/api";
 
 const TransactionsPage = () => {
   const router = useRouter();
-  const transactionData = api.transactions.getAll.useQuery();
+  const { startOfMonth, endOfMonth } = useMonthContext();
+  const transactionData = api.transactions.getByMonth.useQuery({
+    startOfMonth,
+    endOfMonth,
+  });
 
   return (
     <>
       <Header title="Transactions" />
       <div className="flex w-full gap-2">
-        <button className="flex h-fit items-center gap-1 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light">
+        <button className="flex h-fit items-center gap-2 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light">
           <FontAwesomeIcon size="sm" icon={faMagnifyingGlass} />
           <span>Search</span>
         </button>
         <button
-          className="flex h-fit items-center gap-1 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light"
+          className="flex h-fit items-center gap-2 rounded-lg bg-secondary-med py-2 px-5 font-bold text-secondary-dark hover:bg-secondary-light hover:text-secondary-med hover:ring hover:ring-secondary-med group-hover:text-secondary-light"
           onClick={() => {
             void router.push("/transactions/categorize");
           }}
