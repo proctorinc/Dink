d69de99681aafd88aa4a070737be1bad5cb5d13d
Matt Proctor <proctormatt0@gmail.com>
2023-03-31 22:34:51 -0700

    Created ButtonBar component

---
+++ src/components/ui/Button/ButtonBar.tsx
@@ -0,0 +1,13 @@
+import { type FC, type ReactNode } from "react";
+
+type ButtonBarProps = {
+  children: ReactNode;
+};
+
+export const ButtonBar: FC<ButtonBarProps> = ({ children }) => {
+  return (
+    <div className="flex w-full items-start justify-start gap-2 overflow-x-scroll">
+      {children}
+    </div>
+  );
+};
