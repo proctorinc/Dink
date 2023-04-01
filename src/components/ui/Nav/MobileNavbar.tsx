66be11f5fd967c597b22030c0eec123bef700f65
Matt Proctor <proctormatt0@gmail.com>
2023-03-31 22:37:47 -0700

    Moved sidebar, navbar, and mobile navbar to Nav folder. Added IconButton

--- src/components/ui/Nav/MobileNavbar.tsx
+++ src/components/ui/Nav/MobileNavbar.tsx
@@ -5,8 +5,8 @@ import {
   faPiggyBank,
   faReceipt,
 } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
+import { IconButton } from "../Button";
 
 const MobileNavbar = () => {
   const router = useRouter();
@@ -23,27 +23,14 @@ const MobileNavbar = () => {
     <nav className="fixed inset-x-0 bottom-0 z-30 flex w-full justify-center px-3 text-primary-light sm:hidden">
       <div className="flex h-full w-full max-w-md flex-col rounded-t-3xl bg-primary-dark/90 px-4 pt-2 pb-10 backdrop-blur-sm">
         <div className="flex items-center justify-around">
-          {routes.map((route) => {
-            const activeStyle =
-              "h-10 w-10 rounded-full bg-primary-light text-primary-med";
-            const inactiveStyle =
-              "h-10 w-10 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med";
-
-            return (
-              <button
-                key={route.path}
-                className={
-                  router.pathname === route.path ? activeStyle : inactiveStyle
-                }
-              >
-                <FontAwesomeIcon
-                  size="xl"
-                  icon={route.icon}
-                  onClick={() => void router.push(route.path)}
-                />
-              </button>
-            );
-          })}
+          {routes.map((route) => (
+            <IconButton
+              key={route.path}
+              icon={route.icon}
+              active={router.pathname === route.path}
+              onClick={() => void router.push(route.path)}
+            />
+          ))}
         </div>
       </div>
     </nav>
---
+++ src/components/ui/Nav/Sidebar.tsx
@@ -0,0 +1,47 @@
+import {
+  faBuildingColumns,
+  faCalendarAlt,
+  faHome,
+  faPiggyBank,
+  faReceipt,
+} from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
+import { IconButton } from "../Button";
+
+const Sidebar = () => {
+  const router = useRouter();
+
+  const routes = [
+    { path: "/accounts", name: "Accounts", icon: faBuildingColumns },
+    { path: "/budget", name: "Budget", icon: faCalendarAlt },
+    { path: "/", name: "Home", icon: faHome },
+    { path: "/funds", name: "Funds", icon: faPiggyBank },
+    { path: "/transactions", name: "Transactions", icon: faReceipt },
+  ];
+
+  return (
+    <aside className="fixed left-[10%] top-1/2 hidden -translate-y-1/2 text-white sm:flex">
+      <div className="flex flex-col gap-3">
+        {routes.map((route) => (
+          <button
+            key={route.path}
+            className="group flex items-center gap-3"
+            onClick={() => void router.push(route.path)}
+          >
+            <IconButton
+              key={route.path}
+              icon={route.icon}
+              active={router.pathname === route.path}
+              onClick={() => void router.push(route.path)}
+            />
+            <span className="invisible select-none font-bold text-primary-light group-hover:visible">
+              {route.name}
+            </span>
+          </button>
+        ))}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
