1f3371ab72ad9f9a4760627bd8dbaa2754251f36
Matt Proctor <proctormatt0@gmail.com>
2023-04-01 11:58:52 -0700

    Updated icon button to include secondary color scheme and sizes

--- src/components/ui/Button/IconButton.tsx
+++ src/components/ui/Button/IconButton.tsx
@@ -5,17 +5,38 @@ import { type MouseEventHandler, type FC } from "react";
 type IconButtonProps = {
   icon: IconDefinition;
   active?: boolean;
+  style?: "secondary";
+  size?: "sm";
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const IconButton: FC<IconButtonProps> = ({ icon, active, onClick }) => {
-  const iconStyle = active
-    ? "bg-primary-light text-primary-med"
-    : "text-primary-light hover:bg-primary-light hover:text-primary-med";
+export const IconButton: FC<IconButtonProps> = ({
+  icon,
+  active,
+  style,
+  size,
+  onClick,
+}) => {
+  const buttonSize = size === "sm" ? "w-8" : "w-10";
+  const iconSize = size === "sm" ? "sm" : "xl";
+  let iconStyle =
+    "text-primary-light hover:bg-primary-light hover:text-primary-med";
+
+  if (active && style === "secondary") {
+    iconStyle = "bg-secondary-light text-secondary-med";
+  } else if (style === "secondary") {
+    iconStyle =
+      "bg-secondary-dark text-secondary-med group-hover:bg-secondary-light group-hover:text-secondary-med";
+  } else if (active) {
+    iconStyle = "bg-primary-light text-primary-med";
+  }
 
   return (
-    <button className={`${iconStyle} h-10 w-10 rounded-full`} onClick={onClick}>
-      <FontAwesomeIcon size="xl" icon={icon} />
+    <button
+      className={`${iconStyle} ${buttonSize} aspect-square rounded-full`}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon size={iconSize} icon={icon} />
     </button>
   );
 };
