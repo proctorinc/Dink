781de78ad14dd1704e211e36c00d863aded217ac
Matt Proctor <proctormatt0@gmail.com>
2023-04-01 22:21:56 -0700

    Added className prop

--- src/components/ui/Card/CardCollapse.tsx
+++ src/components/ui/Card/CardCollapse.tsx
@@ -3,10 +3,11 @@ import { type FC, type ReactNode } from "react";
 export type CardCollapseProps = {
   children: ReactNode;
   open: boolean;
+  className?: string;
 };
 
-const CardCollapse: FC<CardCollapseProps> = ({ open, children }) => {
-  return <>{open && <div>{children}</div>}</>;
+const CardCollapse: FC<CardCollapseProps> = ({ open, children, className }) => {
+  return <>{open && <div className={className}>{children}</div>}</>;
 };
 
 export default CardCollapse;
