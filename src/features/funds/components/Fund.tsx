746f43b99fc1f51b89cfdf9eca50d8a30beb70b9
Matt Proctor <proctormatt0@gmail.com>
2023-04-01 22:22:57 -0700

    Added fund component implementation

--- src/features/funds/components/Fund.tsx
+++ src/features/funds/components/Fund.tsx
@@ -1,5 +1,31 @@
-const Fund = () => {
-  return <div>Fund</div>;
+import { type FC, type MouseEventHandler } from "react";
+import { type Fund, type Prisma } from "@prisma/client";
+import { IconButton } from "~/components/ui/Button";
+import Card from "~/components/ui/Card";
+import { faMoneyBill1 } from "@fortawesome/free-solid-svg-icons";
+import { formatToCurrency, formatToTitleCase } from "~/utils";
+
+type FundProps = {
+  data?: Fund & {
+    amount: Prisma.Decimal;
+  };
+  onClick?: MouseEventHandler<HTMLDivElement>;
+};
+
+const Fund: FC<FundProps> = ({ data: fund, onClick }) => {
+  return (
+    <Card onClick={onClick}>
+      <Card.Header size="xl">
+        <div className="flex items-center gap-3">
+          <IconButton icon={faMoneyBill1} size="sm" style="secondary" />
+          <h3 className="text-lg font-bold">{formatToTitleCase(fund?.name)}</h3>
+        </div>
+        <span className="text-lg font-bold text-primary-light group-hover:text-primary-med">
+          {formatToCurrency(fund?.amount)}
+        </span>
+      </Card.Header>
+    </Card>
+  );
 };
 
 export default Fund;
