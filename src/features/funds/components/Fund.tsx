7ddccda31665d743fcd0080bb77f24e026180bf3
Matt Proctor <proctormatt0@gmail.com>
2023-04-02 21:53:14 -0700

    Added icons to funds

--- src/features/funds/components/Fund.tsx
+++ src/features/funds/components/Fund.tsx
@@ -2,8 +2,9 @@ import { type FC, type MouseEventHandler } from "react";
 import { type Fund, type Prisma } from "@prisma/client";
 import { IconButton } from "~/components/ui/Button";
 import Card from "~/components/ui/Card";
-import { faMoneyBill1 } from "@fortawesome/free-solid-svg-icons";
+import { faPiggyBank } from "@fortawesome/free-solid-svg-icons";
 import { formatToCurrency, formatToTitleCase } from "~/utils";
+import useIcons from "~/hooks/useIcons";
 
 type FundProps = {
   data?: Fund & {
@@ -13,14 +14,17 @@ type FundProps = {
 };
 
 const Fund: FC<FundProps> = ({ data: fund, onClick }) => {
+  const { convertToIcon } = useIcons();
+  const icon = convertToIcon(fund?.icon) ?? faPiggyBank;
+
   return (
     <Card onClick={onClick}>
       <Card.Header size="xl">
-        <div className="flex items-center gap-3">
-          <IconButton icon={faMoneyBill1} size="sm" style="secondary" />
+        <Card.Group horizontal size="xl">
+          <IconButton icon={icon} size="sm" style="secondary" />
           <h3 className="text-lg font-bold">{formatToTitleCase(fund?.name)}</h3>
-        </div>
-        <span className="text-lg font-bold text-primary-light group-hover:text-primary-med">
+        </Card.Group>
+        <span className="h-fit text-lg font-bold text-primary-light group-hover:text-primary-med">
           {formatToCurrency(fund?.amount)}
         </span>
       </Card.Header>
