70d319aef269951993eeb669ef844cf9b6823cc3
Matt Proctor <proctormatt0@gmail.com>
2023-04-02 21:54:04 -0700

    Added pie chart for budget totals

--- src/pages/budget/index.tsx
+++ src/pages/budget/index.tsx
@@ -1,7 +1,6 @@
 import {
   faChevronLeft,
   faChevronRight,
-  faGear,
   faPlus,
   faRedo,
 } from "@fortawesome/free-solid-svg-icons";
@@ -16,8 +15,11 @@ import Spinner from "~/components/ui/Spinner";
 import { formatToCurrency } from "~/utils";
 import { api } from "~/utils/api";
 import Budget from "~/features/budgets";
+import { useRouter } from "next/router";
+import { PieChart } from "~/components/ui/Charts";
 
 export default function Budgets() {
+  const router = useRouter();
   const {
     month,
     year,
@@ -40,37 +42,45 @@ export default function Budgets() {
     userId: "",
   };
 
+  const chartData = [
+    { name: "Spent", amount: budgetData.data?.spent },
+    { name: "Left", amount: budgetData.data?.leftover },
+  ];
+
   return (
     <>
       <Header title={`Budget`} subtitle={`${month} ${year}`} />
-      <div className="flex w-full items-center justify-center gap-2">
+      <div className="flex w-full items-center justify-center">
         <div className="p-2">
-          <button className="invisible h-8 w-8 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
+          <button className="h-8 w-8 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
             <FontAwesomeIcon icon={faChevronLeft} />
           </button>
         </div>
-        <div className="flex w-full w-3/5 items-center justify-center">
-          <div className="relative flex aspect-square w-full items-center justify-center rounded-full bg-primary-med">
-            <div className="z-20 flex aspect-square w-[75%] flex-col items-center justify-center gap-1 rounded-full bg-primary-dark">
-              <h2 className="text-3xl font-bold">Overall</h2>
-              <span className="text-center text-sm text-primary-light">
-                {formatToCurrency(budgetData.data?.spent)} /{" "}
-                {formatToCurrency(budgetData.data?.goal)}
-              </span>
-            </div>
-            <div className="absolute bottom-0 h-[50%] w-full rounded-bl-full rounded-br-full bg-gradient-to-t from-secondary-dark to-secondary-med" />
-            <div className="absolute right-0 h-full w-[50%] rounded-br-full rounded-tr-full bg-gradient-to-t from-secondary-dark to-secondary-med" />
+        <div className="relative flex h-52 w-full flex-col items-center justify-center pb-5">
+          <div className="absolute flex flex-col items-center justify-center text-xl font-bold">
+            <h2 className="text-xl font-bold">Spending</h2>
           </div>
+          <PieChart data={chartData} progress />
+          <span className="absolute bottom-0 font-bold text-primary-light">
+            {formatToCurrency(budgetData.data?.spent)} of{" "}
+            {formatToCurrency(budgetData.data?.goal)}
+          </span>
         </div>
         <div className="p-2">
-          <button className="invisible h-8 w-8 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
+          <button className="h-8 w-8 rounded-full text-primary-light hover:bg-primary-light hover:text-primary-med">
             <FontAwesomeIcon icon={faChevronRight} />
           </button>
         </div>
       </div>
       <ButtonBar>
-        <Button icon={faGear} />
-        {isCurrentMonth && <Button title="Budget" icon={faPlus} active />}
+        {isCurrentMonth && (
+          <Button
+            title="Budget"
+            icon={faPlus}
+            active
+            onClick={() => void router.push("/budget/create")}
+          />
+        )}
         {!isCurrentMonth && (
           <Button
             title="Current"
