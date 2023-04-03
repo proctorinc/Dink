6a27df2905d0b27fea74226cdb70b48c4bacb52d
Matt Proctor <proctormatt0@gmail.com>
2023-04-02 21:55:23 -0700

    Added icons to individual funds

--- src/pages/funds/[fundId]/index.tsx
+++ src/pages/funds/[fundId]/index.tsx
@@ -4,9 +4,13 @@ import Header from "~/components/ui/Header";
 import Spinner from "~/components/ui/Spinner";
 import { formatToCurrency } from "~/utils";
 import { api } from "~/utils/api";
+import useIcons from "~/hooks/useIcons";
+import { faArrowRight, faMoneyBill1 } from "@fortawesome/free-solid-svg-icons";
+import Card from "~/components/ui/Card";
 
 const FundPage = () => {
   const router = useRouter();
+  const { convertToIcon } = useIcons();
   const { fundId } = router.query;
   const strFundId = typeof fundId === "string" ? fundId : null;
   const fundData = api.funds.getById.useQuery(
@@ -31,15 +35,22 @@ const FundPage = () => {
       <Header
         back
         title={fundData?.data?.name}
+        icon={convertToIcon(fundData?.data?.icon) ?? faMoneyBill1}
         subtitle={`Total: ${formatToCurrency(fundData?.data?.amount)}`}
       />
       <div className="w-full">
         <h2 className="text-left text-xl text-primary-light">Transactions</h2>
       </div>
       {!fundData?.data?.source_transactions?.length && (
-        <div className="group flex w-full items-center justify-between rounded-xl bg-primary-med px-4 py-2">
-          None
-        </div>
+        <Card onClick={() => void router.push("/funds/allocate")}>
+          <Card.Body size="sm" horizontal>
+            <Card.Action
+              title="No Transactions"
+              actionText="Allocate"
+              actionIcon={faArrowRight}
+            />
+          </Card.Body>
+        </Card>
       )}
       <div className="flex w-full flex-col gap-3">
         {fundData.data &&
