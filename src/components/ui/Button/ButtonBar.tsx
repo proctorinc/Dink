741ba184dd70bd9a185410a4b32f2b1c1f8e661c
Matt Proctor <proctormatt0@gmail.com>
2023-04-02 21:07:38 -0700

    Added padding to bar

--- src/components/ui/Button/ButtonBar.tsx
+++ src/components/ui/Button/ButtonBar.tsx
@@ -6,7 +6,7 @@ type ButtonBarProps = {
 
 export const ButtonBar: FC<ButtonBarProps> = ({ children }) => {
   return (
-    <div className="flex w-full items-start justify-start gap-2 overflow-y-visible overflow-x-scroll">
+    <div className="-m-1 flex w-full items-start justify-start gap-2 overflow-y-visible overflow-x-scroll p-1">
       {children}
     </div>
   );
