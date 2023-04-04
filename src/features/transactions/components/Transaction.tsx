ad9471e5ca4d96097f0ccc7c3a81fda81ae463bb
Matt Proctor <proctormatt0@gmail.com>
2023-04-04 16:38:51 -0700

    Updated uncategorized transaction UI

--- src/features/transactions/components/Transaction.tsx
+++ src/features/transactions/components/Transaction.tsx
@@ -3,7 +3,7 @@ import { type Budget, type Fund, Transaction } from "@prisma/client";
 import { useRouter } from "next/router";
 import { type FC } from "react";
 import { formatToCurrency } from "~/utils";
-import { IconButton } from "~/components/ui/Button";
+import Button from "~/components/ui/Button";
 import Card from "~/components/ui/Card";
 
 type TransactionProps = {
@@ -25,9 +25,20 @@ const Transaction: FC<TransactionProps> = ({ data: transaction }) => {
     <Card size="sm" key={transaction.id} onClick={handleOnClick}>
       <Card.Body horizontal>
         <Card.Group size="sm">
-          <span className="text-lg font-bold">{transaction.name}</span>
+          <span className="overflow-x-hidden text-ellipsis text-lg font-bold">
+            {transaction.name}
+          </span>
           <span className="text-sm text-primary-light group-hover:text-primary-med">
-            {transaction.sourceType ? transaction.sourceType : "Uncategorized"}
+            {transaction.sourceType ? (
+              transaction.sourceType
+            ) : (
+              <Button
+                icon={faTriangleExclamation}
+                title="Uncategorized"
+                size="sm"
+                active
+              />
+            )}
             {transaction.sourceType === "fund" &&
               ` / ${transaction.fundSource?.name ?? ""}`}
             {transaction.sourceType === "budget" &&
@@ -46,13 +57,6 @@ const Transaction: FC<TransactionProps> = ({ data: transaction }) => {
               })}
             </span>
           </Card.Group>
-          {!transaction.sourceType && (
-            <IconButton
-              icon={faTriangleExclamation}
-              size="sm"
-              style="secondary"
-            />
-          )}
         </Card.Group>
       </Card.Body>
     </Card>
