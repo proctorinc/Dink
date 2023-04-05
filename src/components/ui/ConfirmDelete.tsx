54f862d9244140c880fda3d0733400696b4b244f
Matt Proctor <proctormatt0@gmail.com>
2023-04-05 12:54:38 -0700

    Add confirm delete for deleting items

---
+++ src/components/ui/ConfirmDelete.tsx
@@ -0,0 +1,79 @@
+import {
+  faExclamationTriangle,
+  faTrash,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { type FC, useState } from "react";
+import Button from "./Button";
+import Card from "./Card";
+
+type ConfirmDeleteProps = {
+  confirmationText: string;
+  onDelete: () => void;
+};
+
+const ConfirmDelete: FC<ConfirmDeleteProps> = ({
+  confirmationText,
+  onDelete,
+}) => {
+  const [clickedDelete, setClickedDelete] = useState(false);
+  const [text, setText] = useState("");
+
+  const isMatchingConfirmationText = text === confirmationText;
+
+  const handleDeleteConfirmed = () => {
+    onDelete();
+    setClickedDelete(false);
+    setText("");
+  };
+
+  return (
+    <>
+      {!clickedDelete && (
+        <Button
+          title="Delete"
+          icon={faTrash}
+          style="danger"
+          onClick={() => setClickedDelete(true)}
+        />
+      )}
+      {clickedDelete && (
+        <Card>
+          <Card.Body>
+            <Card.Group>
+              <label htmlFor="name-input" className="text-primary-light">
+                Enter{" "}
+                <span className="font-bold text-white">{confirmationText}</span>{" "}
+                to delete
+              </label>
+              <div className="w-full pb-3">
+                <input
+                  id="name-input"
+                  placeholder="Enter name..."
+                  className="w-full bg-primary-med text-xl font-bold text-primary-light placeholder-primary-light"
+                  value={text}
+                  onChange={(event) => setText(event.target.value)}
+                />
+              </div>
+              <Button
+                title="Delete"
+                style="danger"
+                icon={faExclamationTriangle}
+                onClick={handleDeleteConfirmed}
+                disabled={!isMatchingConfirmationText}
+              />
+              <Button
+                title="Cancel"
+                icon={faXmark}
+                style="secondary"
+                onClick={() => setClickedDelete(false)}
+              />
+            </Card.Group>
+          </Card.Body>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default ConfirmDelete;
