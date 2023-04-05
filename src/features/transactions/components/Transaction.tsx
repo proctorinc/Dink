d2597a6ab41eefc27932bb5cf95e504881c0918e
Matt Proctor <proctormatt0@gmail.com>
2023-04-05 13:00:42 -0700

    Fixed transaction UI for uncategorized transactions

--- src/features/transactions/components/Transaction.tsx
+++ src/features/transactions/components/Transaction.tsx
@@ -3,7 +3,7 @@ import { type Budget, type Fund, Transaction } from "@prisma/client";
 import { useRouter } from "next/router";
 import { type FC } from "react";
 import { formatToCurrency } from "~/utils";
-import Button from "~/components/ui/Button";
+import { IconButton } from "~/components/ui/Button";
 import Card from "~/components/ui/Card";
 
 type TransactionProps = {
@@ -23,40 +23,40 @@ const Transaction: FC<TransactionProps> = ({ data: transaction }) => {
 
   return (
     <Card size="sm" key={transaction.id} onClick={handleOnClick}>
-      <Card.Body horizontal>
-        <Card.Group size="sm">
-          <span className="overflow-x-hidden text-ellipsis text-lg font-bold">
-            {transaction.name}
-          </span>
-          <span className="text-sm text-primary-light group-hover:text-primary-med">
-            {transaction.sourceType ? (
-              transaction.sourceType
-            ) : (
-              <Button
-                icon={faTriangleExclamation}
-                title="Uncategorized"
-                size="sm"
-                active
-              />
-            )}
-            {transaction.sourceType === "fund" &&
-              ` / ${transaction.fundSource?.name ?? ""}`}
-            {transaction.sourceType === "budget" &&
-              ` / ${transaction.budgetSource?.name ?? ""}`}
-          </span>
-        </Card.Group>
+      <Card.Body horizontal size="sm">
         <Card.Group horizontal>
-          <Card.Group size="sm" className="text-right">
-            <span className="text-lg font-bold group-hover:text-primary-med">
-              {formatToCurrency(transaction.amount)}
+          {transaction.sourceType === null && (
+            <IconButton
+              size="sm"
+              style="secondary"
+              icon={faTriangleExclamation}
+            />
+          )}
+          <Card.Group size="sm">
+            <span className="overflow-x-hidden text-ellipsis text-lg font-bold">
+              {transaction.name}
             </span>
             <span className="text-sm text-primary-light group-hover:text-primary-med">
-              {transaction.date.toLocaleString("en-us", {
-                month: "short",
-                day: "numeric",
-              })}
+              {transaction.sourceType
+                ? transaction.sourceType
+                : "Uncategorized"}
+              {transaction.sourceType === "fund" &&
+                ` / ${transaction.fundSource?.name ?? ""}`}
+              {transaction.sourceType === "budget" &&
+                ` / ${transaction.budgetSource?.name ?? ""}`}
             </span>
           </Card.Group>
+        </Card.Group>
+        <Card.Group size="sm" className="text-right">
+          <span className="text-lg font-bold group-hover:text-primary-med">
+            {formatToCurrency(transaction.amount)}
+          </span>
+          <span className="text-sm text-primary-light group-hover:text-primary-med">
+            {transaction.date.toLocaleString("en-us", {
+              month: "short",
+              day: "numeric",
+            })}
+          </span>
         </Card.Group>
       </Card.Body>
     </Card>
