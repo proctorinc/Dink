5b1d1e25e0e97517a14c73a638081f012695cf6d
Matt Proctor <proctormatt0@gmail.com>
2023-04-05 17:25:33 -0700

    Fixed UI login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,11 @@
+import {
+  faArrowRight,
+  faCircleHalfStroke,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Button from "~/components/ui/Button";
 
 const Login = () => {
   const { data } = useSession();
@@ -10,16 +16,26 @@ const Login = () => {
   }
 
   return (
-    <>
-      <div className="flex flex-col items-center gap-2">
-        <button
-          className="rounded-xl bg-primary-med py-2 px-5 font-bold text-primary-light hover:bg-primary-light hover:text-primary-med hover:ring hover:ring-primary-med"
-          onClick={() => void signIn()}
-        >
-          Login With Google
-        </button>
+    <div className="flex flex-col items-center gap-5 pt-52">
+      <div
+        className="flex items-center gap-3 rounded-xl pb-10 text-7xl font-bold text-primary-light"
+        onClick={() => void router.push("/")}
+      >
+        <FontAwesomeIcon
+          className="h-[52px] w-[52px]"
+          icon={faCircleHalfStroke}
+        />
+        <span>Dink</span>
       </div>
-    </>
+      <Button
+        title="Login"
+        className="w-48"
+        style="secondary"
+        icon={faArrowRight}
+        onClick={() => void signIn()}
+        iconRight
+      />
+    </div>
   );
 };
 
