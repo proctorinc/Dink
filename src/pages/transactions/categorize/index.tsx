16a6bd318cc73c886b70fa1d4dd54d977090baba
Matt Proctor <proctormatt0@gmail.com>
2023-04-06 18:13:33 -0700

    Added loader and income category

--- src/pages/transactions/categorize/index.tsx
+++ src/pages/transactions/categorize/index.tsx
@@ -2,6 +2,7 @@ import {
   faCalendarAlt,
   faPiggyBank,
   faRedo,
+  faSackDollar,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { useMonthContext } from "~/hooks/useMonthContext";
@@ -12,6 +13,7 @@ import Card from "~/components/ui/Card";
 import Budget from "~/features/budgets";
 import Fund from "~/features/funds";
 import Button, { IconButton } from "~/components/ui/Button";
+import Spinner from "~/components/ui/Spinner";
 
 const CategorizePage = () => {
   const [type, setType] = useState<string | null>(null);
@@ -24,82 +26,117 @@ const CategorizePage = () => {
   });
   const uncategorizedTransactions =
     api.transactions.getUncategorized.useQuery();
-  const categorizeTransaction =
-    api.transactions.categorizeTransaction.useMutation({
-      onSuccess: () => ctx.invalidate(),
-    });
+  const categorizeAsBudget = api.transactions.categorizeAsBudget.useMutation({
+    onSuccess: () => ctx.invalidate(),
+  });
+  const categorizeAsFund = api.transactions.categorizeAsFund.useMutation({
+    onSuccess: () => ctx.invalidate(),
+  });
+  const categorizeAsIncome = api.transactions.categorizeAsIncome.useMutation({
+    onSuccess: () => ctx.invalidate(),
+  });
 
   const current = uncategorizedTransactions?.data
     ? uncategorizedTransactions?.data[0]
     : null;
 
   const selectById = (sourceId: string) => {
-    categorizeTransaction.mutate({
-      id: current?.id ?? "",
-      type,
-      sourceId,
-    });
+    if (type === "fund") {
+      categorizeAsFund.mutate({
+        id: current?.id ?? "",
+        sourceId,
+      });
+    } else if (type === "budget") {
+      categorizeAsBudget.mutate({
+        id: current?.id ?? "",
+        sourceId,
+      });
+    }
     setType(null);
   };
+
+  const selectIncome = () => {
+    categorizeAsIncome.mutate({ id: current?.id ?? "" });
+  };
+
+  if (!uncategorizedTransactions.data) {
+    return <Spinner />;
+  }
 
   return (
     <>
       <Header back title="Categorize" />
-
       <div className="flex w-full flex-col gap-2">
         <h2 className="text-left text-xl text-primary-light">Transaction:</h2>
-        {!!uncategorizedTransactions.data && (
-          <NoSourceTransaction data={uncategorizedTransactions.data[0]} />
+        {uncategorizedTransactions?.data?.length > 0 && (
+          <NoSourceTransaction data={uncategorizedTransactions?.data[0]} />
         )}
       </div>
-      <Card>
-        <Card.Header>
-          <h3>Choose a {type ?? "category"}:</h3>
-        </Card.Header>
-        <Card.Collapse open={!type}>
-          <Card onClick={() => setType("fund")}>
-            <Card.Header size="xl">
-              <Card.Group horizontal>
-                <IconButton icon={faPiggyBank} style="secondary" />
-                <h3>Fund</h3>
-              </Card.Group>
+      {uncategorizedTransactions.data.length === 0 && (
+        <Card>
+          <Card.Header size="xl">No Transactions</Card.Header>
+        </Card>
+      )}
+      {uncategorizedTransactions.data.length > 0 && (
+        <>
+          <Card>
+            <Card.Header>
+              <h3>Choose a {type ?? "category"}:</h3>
             </Card.Header>
+            <Card.Collapse open={!type}>
+              <Card onClick={() => setType("fund")}>
+                <Card.Header size="xl">
+                  <Card.Group horizontal>
+                    <IconButton icon={faPiggyBank} style="secondary" />
+                    <h3>Fund</h3>
+                  </Card.Group>
+                </Card.Header>
+              </Card>
+              <Card onClick={() => setType("budget")}>
+                <Card.Header size="xl">
+                  <Card.Group horizontal>
+                    <IconButton icon={faCalendarAlt} style="secondary" />
+                    <h3>Budget</h3>
+                  </Card.Group>
+                </Card.Header>
+              </Card>
+              <Card onClick={selectIncome}>
+                <Card.Header size="xl">
+                  <Card.Group horizontal>
+                    <IconButton icon={faSackDollar} style="secondary" />
+                    <h3>Income</h3>
+                  </Card.Group>
+                </Card.Header>
+              </Card>
+            </Card.Collapse>
+            <Card.Collapse open={type === "budget"} className="rounded-xl">
+              {budgetData?.data?.budgets.spending.map((budget) => (
+                <Budget
+                  key={budget.id}
+                  data={budget}
+                  onClick={() => selectById(budget.id)}
+                />
+              ))}
+            </Card.Collapse>
+            <Card.Collapse open={type === "fund"} className="rounded-xl">
+              {fundsData?.data?.funds.map((fund) => (
+                <Fund
+                  key={fund.id}
+                  data={fund}
+                  onClick={() => selectById(fund.id)}
+                />
+              ))}
+            </Card.Collapse>
           </Card>
-          <Card onClick={() => setType("budget")}>
-            <Card.Header size="xl">
-              <Card.Group horizontal>
-                <IconButton icon={faCalendarAlt} style="secondary" />
-                <h3>Budget</h3>
-              </Card.Group>
-            </Card.Header>
-          </Card>
-        </Card.Collapse>
-        <Card.Collapse open={type === "budget"} className="rounded-xl">
-          {budgetData?.data?.budgets.spending.map((budget) => (
-            <Budget
-              key={budget.id}
-              data={budget}
-              onClick={() => selectById(budget.id)}
-            />
-          ))}
-        </Card.Collapse>
-        <Card.Collapse open={type === "fund"} className="rounded-xl">
-          {fundsData?.data?.funds.map((fund) => (
-            <Fund
-              key={fund.id}
-              data={fund}
-              onClick={() => selectById(fund.id)}
-            />
-          ))}
-        </Card.Collapse>
-      </Card>
-      <Button
-        title="Undo"
-        icon={faRedo}
-        style="secondary"
-        disabled={!type}
-        onClick={() => setType(null)}
-      />
+          <Button
+            title="Reselect"
+            icon={faRedo}
+            style="secondary"
+            disabled={!type}
+            onClick={() => setType(null)}
+          />
+        </>
+      )}
     </>
   );
 };
