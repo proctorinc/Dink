046fff85301eeca3559d15fd49ebce173d12b81e
Matt Proctor <proctormatt0@gmail.com>
2023-04-07 12:49:19 -0700

    Created editable title for updating item names

---
+++ src/components/ui/EditableTitle.tsx
@@ -0,0 +1,71 @@
+import {
+  faPencil,
+  faSquareCheck,
+  faSquareXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState, type FC } from "react";
+import { IconButton } from "./Button";
+import Card from "./Card";
+import Spinner from "./Spinner";
+
+type EditableTitleProps = {
+  value?: string;
+  onUpdate: (updatedValue: string) => void;
+};
+
+const EditableTitle: FC<EditableTitleProps> = ({
+  value: initialValue,
+  onUpdate,
+}) => {
+  const [value, setValue] = useState(initialValue);
+  const [editing, setEditing] = useState(false);
+
+  const handleUpdate = (updatedValue?: string) => {
+    if (updatedValue) {
+      onUpdate(updatedValue);
+      setEditing(false);
+    }
+  };
+
+  if (value === null) {
+    return <Spinner />;
+  }
+
+  if (editing) {
+    return (
+      <Card size="sm">
+        <Card.Body horizontal>
+          <input
+            id="input"
+            placeholder={"Enter name..."}
+            className="bg-primary-med text-xl font-bold text-primary-light placeholder-primary-light"
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+          {!!value && value.length > 0 && (
+            <IconButton
+              icon={faSquareCheck}
+              size="xs"
+              onClick={() => handleUpdate(value)}
+            />
+          )}
+          {value?.length === 0 && (
+            <IconButton
+              icon={faSquareXmark}
+              size="xs"
+              onClick={() => setEditing(false)}
+            />
+          )}
+        </Card.Body>
+      </Card>
+    );
+  }
+  return (
+    <div className="flex gap-2">
+      <h3>{value}</h3>
+      <IconButton icon={faPencil} size="xs" onClick={() => setEditing(true)} />
+    </div>
+  );
+};
+
+export default EditableTitle;
