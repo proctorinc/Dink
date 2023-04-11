06c785fa46596aa0468f9eb3773816981b16de1c
Matt Proctor <proctormatt0@gmail.com>
2023-04-10 23:17:52 -0700

    Added bar charts to budgets

--- src/pages/budget/index.tsx
+++ src/pages/budget/index.tsx
@@ -19,6 +19,7 @@ import { useRouter } from "next/router";
 import { PieChart } from "~/components/ui/Charts";
 import Page from "~/components/ui/Page";
 import { useState } from "react";
+import { BarChart } from "~/components/ui/Charts/BarChart";
 
 export default function Budgets() {
   const router = useRouter();
@@ -47,12 +48,28 @@ export default function Budgets() {
     { name: "Left", amount: budgetData.data?.overall.leftover },
   ];
   const spendingData = [
-    { name: "Spent", amount: budgetData.data?.spending.total },
-    { name: "Budget", amount: budgetData.data?.spending.leftover },
+    {
+      spent: Number(budgetData.data?.spending.total),
+      goal: Number(budgetData.data?.spending.leftover),
+    },
   ];
   const savingsData = [
-    { name: "Saved", amount: budgetData.data?.savings.total },
-    { name: "Goal", amount: budgetData.data?.savings.leftover },
+    {
+      savings: Number(budgetData.data?.savings.total),
+      goal: Number(budgetData.data?.savings.leftover),
+    },
+  ];
+  const barGraphData = [
+    {
+      title: "Spending",
+      amount: Number(budgetData.data?.spending.total),
+      goal: Number(budgetData.data?.spending.leftover),
+    },
+    {
+      title: "Savings",
+      amount: Number(budgetData.data?.savings.total),
+      goal: Number(budgetData.data?.savings.leftover),
+    },
   ];
 
   return (
@@ -60,9 +77,9 @@ export default function Budgets() {
       <Header title={`Budget`} subtitle={`${month} ${year}`} />
       <div className="flex w-full items-center justify-center">
         <IconButton
-          className={chartNum !== 1 ? "invisible" : ""}
+          className={chartNum == 0 ? "invisible" : ""}
           icon={faChevronLeft}
-          onClick={() => setChartNum(0)}
+          onClick={() => setChartNum((prev) => prev - 1)}
         />
         {chartNum === 0 && (
           <div className="relative flex h-52 w-full flex-col items-center justify-center pb-5">
@@ -77,6 +94,41 @@ export default function Budgets() {
           </div>
         )}
         {chartNum === 1 && (
+          <div className="flex h-52 w-full items-center">
+            <div className="relative flex h-40 w-full flex-col items-center justify-center pb-5">
+              <BarChart
+                data={barGraphData}
+                keys={["amount", "goal"]}
+                floatRight
+              />
+              <div className="absolute bottom-0 flex w-full justify-around font-bold text-primary-light">
+                <span>Spending</span>
+                <span>Savings</span>
+              </div>
+            </div>
+          </div>
+        )}
+        {chartNum === 2 && (
+          <div className="flex h-52 w-full items-center">
+            <div className="relative flex h-40 w-full flex-col items-center justify-center pb-5">
+              <BarChart
+                data={spendingData}
+                keys={["spent", "goal"]}
+                floatRight
+              />
+              <span className="absolute bottom-0 font-bold text-primary-light">
+                {formatToCurrency(budgetData.data?.spending.total)}
+              </span>
+            </div>
+            <div className="relative flex h-40 w-full flex-col items-center justify-center pb-5">
+              <BarChart data={savingsData} keys={["savings", "goal"]} />
+              <span className="absolute bottom-0 font-bold text-primary-light">
+                {formatToCurrency(budgetData.data?.savings.total)}
+              </span>
+            </div>
+          </div>
+        )}
+        {chartNum === 3 && (
           <div className="flex h-52 w-full items-center">
             <div className="relative flex h-40 w-full flex-col items-center justify-center pb-5">
               <div className="absolute flex flex-col items-center justify-center text-xl font-bold">
@@ -99,9 +151,9 @@ export default function Budgets() {
           </div>
         )}
         <IconButton
-          className={chartNum !== 0 ? "invisible" : ""}
+          className={chartNum === 3 ? "invisible" : ""}
           icon={faChevronRight}
-          onClick={() => setChartNum(1)}
+          onClick={() => setChartNum((prev) => prev + 1)}
         />
       </div>
       <ButtonBar>
