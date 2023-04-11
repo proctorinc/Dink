962df2aad7ba588b3af71ac2694874946f4783a9
Matt Proctor <proctormatt0@gmail.com>
2023-04-10 17:06:26 -0700

    Added linechart

--- src/pages/funds/[fundId]/index.tsx
+++ src/pages/funds/[fundId]/index.tsx
@@ -8,6 +8,7 @@ import useIcons from "~/hooks/useIcons";
 import {
   faAngleUp,
   faArrowRight,
+  faCoins,
   faGear,
   faMoneyBill1,
 } from "@fortawesome/free-solid-svg-icons";
@@ -17,6 +18,8 @@ import { useState } from "react";
 import ConfirmDelete from "~/components/ui/ConfirmDelete";
 import EditableTitle from "~/components/ui/EditableTitle";
 import Page from "~/components/ui/Page";
+import { LineChart } from "~/components/ui/Charts";
+import { type Serie } from "@nivo/line";
 
 const FundPage = () => {
   const router = useRouter();
@@ -45,6 +48,24 @@ const FundPage = () => {
     updateFundName.mutate({ fundId: fund?.id ?? "", name });
   };
 
+  const data: Serie[] = [
+    {
+      id: "Line",
+      data: [
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 3 },
+        { x: 4, y: 2 },
+        { x: 5, y: 5 },
+        { x: 6, y: 6 },
+        { x: 7, y: 8 },
+        { x: 8, y: 5 },
+        { x: 9, y: 9 },
+        { x: 10, y: 9 },
+      ],
+    },
+  ];
+
   if (fundData.isLoading) {
     return <Spinner />;
   }
@@ -54,60 +75,75 @@ const FundPage = () => {
   }
 
   return (
-    <Page auth title="Fund">
-      <Header
-        back
-        title={<EditableTitle value={fund?.name} onUpdate={handleNameUpdate} />}
-        icon={convertToIcon(fund?.icon) ?? faMoneyBill1}
-        subtitle={`Total: ${formatToCurrency(fundData?.data?.amount)}`}
-      />
-      <ButtonBar>
-        <Button
-          icon={settingsOpen ? faAngleUp : faGear}
-          onClick={() => setSettingsOpen((prev) => !prev)}
+    <Page auth title="Fund" style="basic">
+      <div className="w-full px-4">
+        <Header
+          back
+          title={
+            <EditableTitle value={fund?.name} onUpdate={handleNameUpdate} />
+          }
+          icon={convertToIcon(fund?.icon) ?? faMoneyBill1}
+          subtitle={`Total: ${formatToCurrency(fundData?.data?.amount)}`}
         />
-      </ButtonBar>
-      {settingsOpen && (
+      </div>
+      <div className="flex h-40 w-full flex-col">
+        <LineChart data={data} />
+      </div>
+      <div className="flex w-full flex-col gap-4 px-4">
         <ButtonBar>
-          <ConfirmDelete
-            confirmationText={fund?.name ?? "delete fund"}
-            onDelete={handleDeleteFund}
+          <Button
+            icon={settingsOpen ? faAngleUp : faGear}
+            onClick={() => setSettingsOpen((prev) => !prev)}
+          />
+          <Button
+            title="Allocate"
+            icon={faCoins}
+            style="secondary"
+            onClick={() => void router.push("/funds/allocate")}
           />
         </ButtonBar>
-      )}
-      <div className="w-full">
-        <h2 className="text-left text-xl text-primary-light">Transactions</h2>
-      </div>
-      {fundData?.data?.source_transactions?.length === 0 && (
-        <Card>
-          <Card.Header>
-            <h3>No Transactions</h3>
-          </Card.Header>
-          <Card.Body>
-            <Card.Group>
-              <Button
-                title="Allocate Funds"
-                icon={faArrowRight}
-                style="secondary"
-                iconRight
-                onClick={() => void router.push("/funds/allocate")}
-              />
-              <Button
-                title="Categorize Transactions"
-                icon={faArrowRight}
-                style="secondary"
-                iconRight
-                onClick={() => void router.push("/transactions/categorize")}
-              />
-            </Card.Group>
-          </Card.Body>
-        </Card>
-      )}
-      <div className="flex w-full flex-col gap-3">
-        {fundData.data &&
-          fundData.data.source_transactions.map((transaction) => (
-            <NoSourceTransaction key={transaction.id} data={transaction} />
-          ))}
+        {settingsOpen && (
+          <ButtonBar>
+            <ConfirmDelete
+              confirmationText={fund?.name ?? "delete fund"}
+              onDelete={handleDeleteFund}
+            />
+          </ButtonBar>
+        )}
+        <div className="w-full">
+          <h2 className="text-left text-xl text-primary-light">Transactions</h2>
+        </div>
+        {fundData?.data?.source_transactions?.length === 0 && (
+          <Card>
+            <Card.Header>
+              <h3>No Transactions</h3>
+            </Card.Header>
+            <Card.Body>
+              <Card.Group>
+                <Button
+                  title="Allocate Funds"
+                  icon={faArrowRight}
+                  style="secondary"
+                  iconRight
+                  onClick={() => void router.push("/funds/allocate")}
+                />
+                <Button
+                  title="Categorize Transactions"
+                  icon={faArrowRight}
+                  style="secondary"
+                  iconRight
+                  onClick={() => void router.push("/transactions/categorize")}
+                />
+              </Card.Group>
+            </Card.Body>
+          </Card>
+        )}
+        <div className="flex w-full flex-col gap-3">
+          {fundData.data &&
+            fundData.data.source_transactions.map((transaction) => (
+              <NoSourceTransaction key={transaction.id} data={transaction} />
+            ))}
+        </div>
       </div>
     </Page>
   );
