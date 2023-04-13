251970537967c41d4c0d606d795a41f7f6afa7ed
Matt Proctor <proctormatt0@gmail.com>
2023-04-12 18:49:38 -0700

    Updated Login UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,19 @@
-import {
-  faArrowRight,
-  faCircleHalfStroke,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Button from "~/components/ui/Button";
+import { useState } from "react";
+import { z } from "zod";
+import Button, { IconButton } from "~/components/ui/Button";
+import Card from "~/components/ui/Card";
 import Page from "~/components/ui/Page";
 import Spinner from "~/components/ui/Spinner";
 
 const Login = () => {
   const { data: sessionData } = useSession();
   const router = useRouter();
+  const [email, setEmail] = useState("");
+
+  const isValidEmail = z.string().email().safeParse(email).success;
 
   if (sessionData) {
     void router.push("/");
@@ -20,21 +22,44 @@ const Login = () => {
   if (!sessionData) {
     return (
       <Page title="Login" style="centered">
-        <div className="flex items-center gap-3 rounded-xl pb-40 text-7xl font-bold text-primary-light">
-          <FontAwesomeIcon
-            className="h-[52px] w-[52px]"
-            icon={faCircleHalfStroke}
-          />
-          <span>Dink</span>
+        <div className="mb-20 flex w-full rounded-xl px-12 text-primary-light">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-7xl font-bold">Login</h1>
+            <h3 className="text-3xl font-bold text-primary-med">
+              Let&apos;s get started!
+            </h3>
+          </div>
         </div>
-        <Button
-          title="Login"
-          className="w-48"
-          style="secondary"
-          icon={faArrowRight}
-          onClick={() => void signIn()}
-          iconRight
-        />
+        <div className="flex w-full flex-col gap-4 px-12">
+          <Card>
+            <Card.Body>
+              <Card.Group horizontal>
+                <IconButton size="sm" icon={faEnvelope} />
+                <input
+                  id="email-input"
+                  placeholder={"Enter Email..."}
+                  className="bg-primary-med text-xl font-bold text-primary-light placeholder-primary-light focus:placeholder-primary-med"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </Card.Group>
+            </Card.Body>
+          </Card>
+          <Button
+            title="Request Access"
+            className="w-full"
+            style={isValidEmail ? "secondary" : "primary"}
+            disabled={!isValidEmail}
+            onClick={() => void signIn("google")}
+          />
+          <Button
+            title="Login as Guest"
+            className="w-full"
+            style={isValidEmail ? "primary" : "secondary"}
+            icon={faUser}
+            onClick={() => void signIn("google")}
+          />
+        </div>
       </Page>
     );
   }
