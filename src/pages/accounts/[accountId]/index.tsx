d264d8a7e45cf565d7f2124b71363bb2c96a0589
Matt Proctor <proctormatt0@gmail.com>
2023-04-14 14:33:50 -0700

    Removed query for logo data

--- src/pages/accounts/[accountId]/index.tsx
+++ src/pages/accounts/[accountId]/index.tsx
@@ -24,14 +24,6 @@ const AccountPage = () => {
   const accountData = api.bankAccounts.getById.useQuery({
     accountId: strAccountId ?? "",
   });
-  const institutionQuery = api.plaid.getInstitution.useQuery(
-    {
-      institutionId: accountData.data?.item.institutionId ?? "",
-    },
-    {
-      enabled: !!accountData.data?.item.institutionId,
-    }
-  );
   const deleteAccount = api.bankAccounts.delete.useMutation({
     onSuccess: () => void ctx.invalidate(),
   });
@@ -41,6 +33,8 @@ const AccountPage = () => {
     void router.push("/accounts");
   };
 
+  const account = accountData.data;
+
   if (accountData.isLoading) {
     return <Spinner />;
   }
@@ -48,29 +42,32 @@ const AccountPage = () => {
   const Icon = () => {
     return (
       <>
-        {!institutionQuery.data && (
-          <div className="flex aspect-square h-10 w-10 items-center justify-center overflow-hidden rounded-full border border-primary-light bg-primary-dark">
-            <IconButton size="xs" icon={faBuildingColumns} />
-          </div>
-        )}
-        {!!institutionQuery.data?.logo && (
-          <Image
-            className="h-10 w-10 rounded-full border border-primary-light"
-            width={100}
-            height={100}
-            src={`data:image/jpeg;base64,${institutionQuery.data.logo}`}
-            alt="user-image"
-          />
-        )}
-        {!institutionQuery.data?.logo && !!institutionQuery.data?.url && (
-          <Image
-            className="h-10 w-10 rounded-full border border-primary-light"
-            width={100}
-            height={100}
-            src={`https://s2.googleusercontent.com/s2/favicons?domain=${institutionQuery.data.url}&sz=256`}
-            alt="user-image"
-          />
-        )}
+        {!account?.item.institution?.logo &&
+          !account?.item.institution?.url && (
+            <div className="flex aspect-square h-10 w-10 items-center justify-center overflow-hidden rounded-full border border-primary-light bg-primary-dark">
+              <IconButton icon={faBuildingColumns} />
+            </div>
+          )}
+        {account?.item.institution?.logo &&
+          account?.item.institution?.logo !== "null" && (
+            <Image
+              className="h-12 w-12 rounded-full border border-primary-light"
+              width={100}
+              height={100}
+              src={`data:image/jpeg;base64,${account?.item.institution.logo.toString()}`}
+              alt="logo"
+            />
+          )}
+        {account?.item.institution?.logo === "null" &&
+          account?.item.institution?.url && (
+            <Image
+              className="w-10 rounded-full border border-primary-light bg-white"
+              width={100}
+              height={100}
+              src={`https://s2.googleusercontent.com/s2/favicons?domain=${account.item.institution.url}&sz=256`}
+              alt="institution-image"
+            />
+          )}
       </>
     );
   };
@@ -80,7 +77,7 @@ const AccountPage = () => {
       <Header
         back
         title={
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Icon />
             <h3>{accountData?.data?.name}</h3>
           </div>
