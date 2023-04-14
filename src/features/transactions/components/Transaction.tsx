cac3219924762618290a7af24e94b471bdcc818d
Matt Proctor <proctormatt0@gmail.com>
2023-04-14 14:33:19 -0700

    Changed styling. Removed exclamation

--- src/features/transactions/components/Transaction.tsx
+++ src/features/transactions/components/Transaction.tsx
@@ -1,12 +1,8 @@
-import {
-  faAngleRight,
-  faTriangleExclamation,
-} from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { type Budget, type Fund, Transaction } from "@prisma/client";
 import { useRouter } from "next/router";
 import { type FC } from "react";
 import { formatToCurrency, formatToTitleCase } from "~/utils";
-import { IconButton } from "~/components/ui/Button";
 import Card from "~/components/ui/Card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -28,38 +24,27 @@ const Transaction: FC<TransactionProps> = ({ data: transaction }) => {
   return (
     <Card size="sm" key={transaction.id} onClick={handleOnClick}>
       <Card.Body horizontal size="sm">
-        <Card.Group horizontal>
-          {transaction.sourceType === null && (
-            <IconButton
-              size="sm"
-              style="secondary"
-              icon={faTriangleExclamation}
-            />
-          )}
-          <Card.Group size="sm">
-            <span className="overflow-x-hidden text-ellipsis text-lg font-bold">
-              {transaction.name}
+        <Card.Group size="sm">
+          <span className="font-bold">{transaction.name}</span>
+          <div className="flex items-center gap-1 text-sm text-primary-light group-hover:text-primary-med">
+            <span className="">
+              {transaction.sourceType
+                ? formatToTitleCase(transaction.sourceType)
+                : "Uncategorized"}
             </span>
-            <div className="flex items-center gap-1 text-sm text-primary-light group-hover:text-primary-med">
-              <span className="">
-                {transaction.sourceType
-                  ? formatToTitleCase(transaction.sourceType)
-                  : "Uncategorized"}
-              </span>
-              {(transaction.sourceType === "budget" ||
-                transaction.sourceType === "fund" ||
-                transaction.sourceType === "savings") && (
-                <>
-                  <FontAwesomeIcon icon={faAngleRight} size="xs" />
-                  <span>
-                    {transaction.sourceType === "budget"
-                      ? transaction.budgetSource?.name
-                      : transaction.fundSource?.name}
-                  </span>
-                </>
-              )}
-            </div>
-          </Card.Group>
+            {(transaction.sourceType === "budget" ||
+              transaction.sourceType === "fund" ||
+              transaction.sourceType === "savings") && (
+              <>
+                <FontAwesomeIcon icon={faAngleRight} size="xs" />
+                <span>
+                  {transaction.sourceType === "budget"
+                    ? transaction.budgetSource?.name
+                    : transaction.fundSource?.name}
+                </span>
+              </>
+            )}
+          </div>
         </Card.Group>
         <Card.Group size="sm" className="text-right">
           <span className="text-lg font-bold group-hover:text-primary-med">
