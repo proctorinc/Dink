5269d30cbf6aca0b0dc2d3c0ca4d654e03a0dba7
Matt Proctor <proctormatt0@gmail.com>
2023-04-20 11:38:44 -0700

    Updated login UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,5 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { z } from "zod";
 import Button from "~/components/ui/Button";
 import Page from "~/components/ui/Page";
 import Spinner from "~/components/ui/Spinner";
@@ -9,9 +7,6 @@ import Spinner from "~/components/ui/Spinner";
 const Login = () => {
   const { data: sessionData } = useSession();
   const router = useRouter();
-  const [email, setEmail] = useState("");
-
-  const isValidEmail = z.string().email().safeParse(email).success;
 
   if (sessionData) {
     void router.push("/");
@@ -20,36 +15,36 @@ const Login = () => {
   if (!sessionData) {
     return (
       <Page title="Login" style="centered">
-        <div className="mb-20 flex w-full rounded-xl px-12 text-primary-light">
+        <div className="mb-10 flex w-full rounded-xl px-12 text-primary-light">
           <div className="flex flex-col gap-2">
-            <h1 className="text-7xl font-bold">Login</h1>
+            <h1 className="text-5xl font-bold">Login</h1>
           </div>
         </div>
         <div className="flex w-full flex-col gap-4 px-12">
-          <input
+          {/* <input
             id="email-input"
             placeholder="Enter Email..."
             className="relative rounded-xl bg-primary-med py-2 px-4 font-bold text-primary-light placeholder-primary-light ring ring-primary-med focus:placeholder-primary-med"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
-          />
+          />*/}
           <Button
-            title="Request Access"
+            title="Login"
             className="w-full"
-            style={isValidEmail ? "secondary" : "primary"}
-            disabled={!isValidEmail}
+            style="secondary"
             onClick={() => void signIn("google")}
           />
           <div className="flex items-center justify-center gap-3 py-2">
             <div className="h-0.5 w-full rounded-full bg-primary-med" />
-            <span className="font-bold text-primary-light">OR</span>
+            <span className="text-xs font-bold text-primary-light">OR</span>
             <div className="h-0.5 w-full rounded-full bg-primary-med" />
           </div>
           <Button
-            title="Login as Guest"
+            title="Try Demo"
             className="w-full"
-            style={isValidEmail ? "primary" : "secondary"}
+            style="primary"
             onClick={() => void signIn("google")}
+            disabled
           />
         </div>
       </Page>
