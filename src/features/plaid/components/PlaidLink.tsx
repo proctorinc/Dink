cd2e793e801d56d5e1aeea0584c41113672781a2
Matt Proctor <proctormatt0@gmail.com>
2023-04-21 16:25:25 -0700

    Added loading and error notifications

--- src/features/plaid/components/PlaidLink.tsx
+++ src/features/plaid/components/PlaidLink.tsx
@@ -1,33 +1,49 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+import { type FC, useEffect, useState } from "react";
 import { type PlaidLinkOnSuccess, usePlaidLink } from "react-plaid-link";
 import Button from "~/components/ui/Button";
+import useNotifications from "~/hooks/useNotifications";
 import { api } from "~/utils/api";
 
-export const PlaidLink = () => {
+export const PlaidLink: FC = () => {
   const ctx = api.useContext();
   const [linkToken, setLinkToken] = useState<string | null>(null);
+
+  const { setLoadingNotification, setErrorNotification } = useNotifications();
+
+  const failedToLinkError = () => {
+    setErrorNotification("Failed to link bank");
+  };
+
   const getLinkToken = api.plaid.getLinkToken.useMutation({
     onSuccess: (token) => setLinkToken(token),
-  });
-  const createPlaidItem = api.plaid.createInstitution.useMutation({
-    onSuccess: () => void ctx.invalidate(),
+    onError: () => failedToLinkError(),
   });
 
-  const handleLinkSuccess: PlaidLinkOnSuccess = (public_token, metadata) => {
+  const handleSuccessfulLink: PlaidLinkOnSuccess = (public_token, metadata) => {
+    setLoadingNotification("Linking bank...");
     if (metadata.institution) {
       createPlaidItem.mutate({
         publicToken: public_token,
         institutionId: metadata.institution.institution_id,
       });
     } else {
-      console.error("Failed to link: Institution does not exist");
+      failedToLinkError();
     }
   };
 
   const { open, ready } = usePlaidLink({
     token: linkToken ?? null,
-    onSuccess: handleLinkSuccess,
+    onSuccess: (public_token, metadata) => {
+      setLinkToken(null);
+      handleSuccessfulLink(public_token, metadata);
+    },
+    onExit: () => setLinkToken(null),
+  });
+
+  const createPlaidItem = api.plaid.createInstitution.useMutation({
+    onSuccess: () => void ctx.invalidate(),
+    onError: () => failedToLinkError(),
   });
 
   useEffect(() => {
