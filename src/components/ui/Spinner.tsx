ff366f10ce1426b1fe93bffb38fc0c7802999c90
Matt Proctor <proctormatt0@gmail.com>
2023-04-21 16:23:08 -0700

    Added styles

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import { type FC } from "react";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { type SizeProp } from "@fortawesome/fontawesome-svg-core";
 
-const Spinner = () => {
+type SpinnerProps = {
+  size?: "sm" | "xs" | "lg";
+};
+
+const Spinner: FC<SpinnerProps> = ({ size }) => {
+  let spinnerSize: SizeProp = "2xl";
+
+  if (size === "sm") {
+    spinnerSize = "lg";
+  } else if (size === "xs") {
+    spinnerSize = "sm";
+  }
+
   return (
-    <div className="flex w-full justify-center py-10">
-      <FontAwesomeIcon
-        className="animate-spin text-primary-light"
-        size="2xl"
-        icon={faSpinner}
-      />
-    </div>
+    <FontAwesomeIcon
+      className="animate-spin text-primary-light"
+      size={spinnerSize}
+      icon={faSpinner}
+    />
   );
 };
 
