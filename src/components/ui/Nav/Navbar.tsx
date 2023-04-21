c0498cef07614b7040004123e77492ef82fc470c
Matt Proctor <proctormatt0@gmail.com>
2023-04-21 16:25:01 -0700

    Adde notifications

--- src/components/ui/Nav/Navbar.tsx
+++ src/components/ui/Nav/Navbar.tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import useNotifications from "~/hooks/useNotifications";
-import { Notification } from "../Notifications/Notification";
+import { Notification } from "../Notification/Notification";
 
 const Navbar = () => {
   const router = useRouter();
