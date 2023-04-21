efcc33e57ff7976139e1c889e285cc40dfd63ecd
Matt Proctor <proctormatt0@gmail.com>
2023-04-21 16:23:30 -0700

    Added notifications

--- src/components/ui/Nav/Navbar.tsx
+++ src/components/ui/Nav/Navbar.tsx
@@ -3,42 +3,50 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import useNotifications from "~/hooks/useNotifications";
+import { Notification } from "../Notifications/Notification";
 
 const Navbar = () => {
   const router = useRouter();
   const { data: sessionData } = useSession();
+  const { type, message } = useNotifications();
 
   return (
-    <nav className="absolute sticky top-0 z-30 flex w-full justify-center bg-primary-dark/90 py-2 pl-2 pr-4 text-primary-light backdrop-blur-sm md:flex">
-      <div className="flex w-full max-w-lg items-center justify-center justify-between sm:max-w-4xl">
-        <button
-          className="flex items-center gap-2 rounded-xl py-1 px-3 text-3xl font-bold"
-          onClick={() => void router.push("/")}
-        >
-          <FontAwesomeIcon
-            className="h-6 w-6 text-primary-light"
-            icon={faCircleHalfStroke}
-          />
-          <span>Dink</span>
-        </button>
-        {sessionData?.user && (
+    <>
+      <nav className="absolute sticky top-0 z-30 flex w-full justify-center bg-primary-dark/90 py-2 pl-2 pr-4 text-primary-light backdrop-blur-sm md:flex">
+        <div className="flex w-full max-w-lg items-center justify-center justify-between sm:max-w-4xl">
           <button
-            className="aspect-square w-10 rounded-full bg-secondary-dark text-secondary-med hover:bg-secondary-med hover:text-secondary-light"
-            onClick={() => void router.push("/profile")}
+            className="flex items-center gap-2 rounded-xl py-1 px-3 text-3xl font-bold"
+            onClick={() => void router.push("/")}
           >
-            {sessionData?.user.image && (
-              <Image
-                className="w-full rounded-full border-2 border-primary-med"
-                width={10}
-                height={10}
-                src={sessionData?.user.image}
-                alt="user-image"
-              />
-            )}
+            <FontAwesomeIcon
+              className="h-6 w-6 text-primary-light"
+              icon={faCircleHalfStroke}
+            />
+            <span>Dink</span>
           </button>
-        )}
+          {sessionData?.user && (
+            <button
+              className="aspect-square w-10 rounded-full bg-secondary-dark text-secondary-med shadow-lg hover:bg-secondary-med hover:text-secondary-light"
+              onClick={() => void router.push("/profile")}
+            >
+              {sessionData?.user.image && (
+                <Image
+                  className="w-full rounded-full border-2 border-primary-med"
+                  width={10}
+                  height={10}
+                  src={sessionData?.user.image}
+                  alt="user-image"
+                />
+              )}
+            </button>
+          )}
+        </div>
+      </nav>
+      <div className="min-h-4">
+        {type && message && <Notification type={type} message={message} />}
       </div>
-    </nav>
+    </>
   );
 };
 
