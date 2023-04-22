ee5c09d963ac99917c1acee4db2ebd81bce5906d
Matt Proctor <proctormatt0@gmail.com>
2023-04-22 13:56:44 -0700

    Added centered layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,25 @@
 import { useRouter } from "next/router";
 import Button from "~/components/ui/Button";
+import Page from "~/components/ui/Page";
 
 const NotFoundPage = () => {
   const router = useRouter();
 
   return (
-    <>
-      <h1 className="pt-64 text-5xl font-extrabold text-white">404</h1>
-      <h1 className="text-5xl font-extrabold text-white">Not Found</h1>
-      <span className="text-md text-primary-light">
-        Oops, looks like you got turned around.
-      </span>
-      <Button
-        title="Go Home"
-        style="secondary"
-        onClick={() => void router.push("/")}
-      />
-    </>
+    <Page title="Not Found" style="centered">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-5xl font-extrabold text-white">404</h1>
+        <h1 className="text-5xl font-extrabold text-white">Not Found</h1>
+        <span className="text-md text-primary-light">
+          Oops, looks like you got turned around.
+        </span>
+        <Button
+          title="Go Home"
+          style="secondary"
+          onClick={() => void router.push("/")}
+        />
+      </div>
+    </Page>
   );
 };
 
