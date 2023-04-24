63d3f13ecf28da08cd0f387a428a2c3bc39e1b9e
Matt Proctor <proctormatt0@gmail.com>
2023-04-23 17:16:01 -0700

    Added custom className styling

--- src/components/ui/Charts/ProgressBar.tsx
+++ src/components/ui/Charts/ProgressBar.tsx
@@ -6,9 +6,15 @@ type ProgressBarProps = {
   value?: Prisma.Decimal;
   goal?: Prisma.Decimal;
   size?: "sm";
+  className?: string;
 };
 
-export const ProgressBar: FC<ProgressBarProps> = ({ value, goal, size }) => {
+export const ProgressBar: FC<ProgressBarProps> = ({
+  value,
+  goal,
+  size,
+  className,
+}) => {
   const percentComplete = formatToProgressPercentage(value, goal);
 
   const barHeight = size === "sm" ? "h-4" : "h-6";
@@ -18,7 +24,9 @@ export const ProgressBar: FC<ProgressBarProps> = ({ value, goal, size }) => {
       className={`${barHeight} relative w-full rounded-md bg-primary-dark shadow-inner group-hover:bg-primary-med`}
     >
       <div
-        className="absolute h-full rounded-md bg-gradient-to-r from-secondary-dark to-secondary-med"
+        className={`absolute h-full rounded-md bg-gradient-to-r from-secondary-dark to-secondary-med ${
+          className ?? ""
+        }`}
         style={{ width: percentComplete }}
       ></div>
     </div>
