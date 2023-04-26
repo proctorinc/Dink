6166a8bc32eed0a10ed356f66e2b8ccdf2dd4ab2
Matt Proctor <proctormatt0@gmail.com>
2023-04-25 18:23:05 -0700

    Added modal for account details

--- src/features/accounts/components/Institution.tsx
+++ src/features/accounts/components/Institution.tsx
@@ -3,6 +3,7 @@ import {
   faAngleUp,
   faBuildingColumns,
   faGear,
+  faToggleOn,
 } from "@fortawesome/free-solid-svg-icons";
 import { type BankAccount } from "@prisma/client";
 import Image from "next/image";
@@ -10,6 +11,7 @@ import React, { type FC, useState } from "react";
 import { IconButton } from "~/components/ui/Button";
 import Card from "~/components/ui/Card";
 import ConfirmDelete from "~/components/ui/ConfirmDelete";
+import Modal from "~/components/ui/Modal";
 import { AccountManage } from "./AccountManage";
 
 type InstitutionProps = {
@@ -27,6 +29,9 @@ type InstitutionProps = {
 export const Institution: FC<InstitutionProps> = ({ data: institution }) => {
   const [settingsOpen, setSettingsOpen] = useState(false);
   const [accountsOpen, setAccountsOpen] = useState(false);
+  const [selectedAccount, setSelectedAccount] = useState<BankAccount | null>(
+    null
+  );
 
   const toggleAccountsOpen = () => {
     setAccountsOpen((prev) => !prev);
@@ -36,75 +41,98 @@ export const Institution: FC<InstitutionProps> = ({ data: institution }) => {
   };
 
   return (
-    <Card key={institution.id}>
-      <Card.Header>
-        <Card.Group horizontal>
-          {!institution?.logo && !institution?.url && (
-            <div className="flex aspect-square h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-primary-dark shadow-xl">
-              <IconButton icon={faBuildingColumns} />
-            </div>
-          )}
-          {institution.logo !== "null" && (
-            <Image
-              className="w-10 rounded-full shadow-xl"
-              width={100}
-              height={100}
-              src={`data:image/jpeg;base64,${
-                institution?.logo?.toString() ?? ""
-              }`}
-              alt="logo"
-            />
-          )}
-          {institution.logo === "null" && institution.url && (
-            <Image
-              className="w-10 rounded-full bg-white shadow-xl"
-              width={100}
-              height={100}
-              src={`https://s2.googleusercontent.com/s2/favicons?domain=${institution.url}&sz=256`}
-              alt="institution-image"
-            />
-          )}
-          <Card.Group size="sm">
-            <h3>{institution.name}</h3>
-            <h3 className="text-sm font-normal text-primary-light">
-              {institution.linkedAccounts.length} Linked Account
-              {institution.linkedAccounts.length === 1 ? "" : "s"}
-            </h3>
+    <>
+      <Card key={institution.id}>
+        <Card.Header>
+          <Card.Group horizontal>
+            {!institution?.logo && !institution?.url && (
+              <div className="flex aspect-square h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-primary-dark shadow-xl">
+                <IconButton icon={faBuildingColumns} />
+              </div>
+            )}
+            {institution.logo !== "null" && (
+              <Image
+                className="w-10 rounded-full shadow-xl"
+                width={100}
+                height={100}
+                src={`data:image/jpeg;base64,${
+                  institution?.logo?.toString() ?? ""
+                }`}
+                alt="logo"
+              />
+            )}
+            {institution.logo === "null" && institution.url && (
+              <Image
+                className="w-10 rounded-full bg-white shadow-xl"
+                width={100}
+                height={100}
+                src={`https://s2.googleusercontent.com/s2/favicons?domain=${institution.url}&sz=256`}
+                alt="institution-image"
+              />
+            )}
+            <Card.Group size="sm">
+              <h3>{institution.name}</h3>
+              <h3 className="text-sm font-normal text-primary-light">
+                {institution.linkedAccounts.length} Linked Account
+                {institution.linkedAccounts.length === 1 ? "" : "s"}
+              </h3>
+            </Card.Group>
           </Card.Group>
-        </Card.Group>
-        <IconButton
-          icon={settingsOpen ? faAngleUp : faGear}
-          noShadow
-          size="sm"
-          onClick={toggleSettingsOpen}
-        />
-      </Card.Header>
-      <Card.Collapse open={settingsOpen}>
-        <Card.Body>
-          <h4 className="font-bold text-primary-light">Settings</h4>
-          <ConfirmDelete
-            confirmationText={`Delete my ${institution.name}`}
-            onDelete={() => console.log("delete")}
-          />
-        </Card.Body>
-      </Card.Collapse>
-      <Card.Body>
-        <div className="flex gap-1" onClick={toggleAccountsOpen}>
           <IconButton
-            icon={accountsOpen ? faAngleUp : faAngleDown}
-            size="xs"
+            icon={settingsOpen ? faAngleUp : faGear}
             noShadow
+            size="sm"
+            onClick={toggleSettingsOpen}
           />
-          <h3 className="w-full text-left text-sm font-bold text-primary-light">
-            {accountsOpen ? "Hide" : "Show"} Accounts
-          </h3>
-        </div>
-      </Card.Body>
-      <Card.Collapse open={accountsOpen}>
-        {institution.linkedAccounts.map((account) => (
-          <AccountManage key={account.id} data={account} />
-        ))}
-      </Card.Collapse>
-    </Card>
+        </Card.Header>
+        <Card.Collapse open={settingsOpen}>
+          <Card.Body>
+            <h4 className="font-bold text-primary-light">Settings</h4>
+            <ConfirmDelete
+              confirmationText={`Delete my ${institution.name}`}
+              onDelete={() => console.log("delete")}
+            />
+          </Card.Body>
+        </Card.Collapse>
+        <Card.Body>
+          <div className="flex gap-1" onClick={toggleAccountsOpen}>
+            <IconButton
+              icon={accountsOpen ? faAngleUp : faAngleDown}
+              size="xs"
+              noShadow
+            />
+            <h3 className="w-full text-left text-sm font-bold text-primary-light">
+              {accountsOpen ? "Hide" : "Show"} Accounts
+            </h3>
+          </div>
+        </Card.Body>
+        <Card.Collapse open={accountsOpen}>
+          {institution.linkedAccounts.map((account) => (
+            <AccountManage
+              key={account.id}
+              data={account}
+              onClick={() => setSelectedAccount(account)}
+            />
+          ))}
+        </Card.Collapse>
+      </Card>
+      {selectedAccount && (
+        <Modal
+          title={selectedAccount.name}
+          onClose={() => setSelectedAccount(null)}
+        >
+          <ConfirmDelete
+            confirmationText={selectedAccount.name}
+            onDelete={() => console.log("Delete Account")}
+          />
+          <Card noShadow size="sm" onClick={() => console.log("ok")}>
+            <Card.Body horizontal>
+              <h3 className="text-lg">Hide from Savings</h3>
+              <IconButton icon={faToggleOn} noShadow />
+            </Card.Body>
+          </Card>
+        </Modal>
+      )}
+    </>
   );
 };
