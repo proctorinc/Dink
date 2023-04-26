129f0e85e4fbff2e7cd94efdd9755bb8711b8215
Matt Proctor <proctormatt0@gmail.com>
2023-04-25 18:22:43 -0700

    Updated modal UI to use Card

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,6 +1,7 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { type FC, type ReactNode } from "react";
+import { IconButton } from "./Button";
+import Card from "./Card";
 
 type ModalProps = {
   children: ReactNode;
@@ -11,15 +12,17 @@ type ModalProps = {
 const Modal: FC<ModalProps> = ({ children, title, onClose }) => {
   return (
     <div
-      className="fixed absolute top-0 z-50 flex h-screen w-full items-center justify-center overflow-y-clip overscroll-auto bg-primary-dark/50 shadow-xl backdrop-blur-sm"
+      className="fixed inset-0 z-50 flex h-screen w-full items-center justify-center overflow-x-hidden overflow-y-hidden overscroll-auto bg-primary-dark/60 text-white shadow-2xl backdrop-blur-sm"
       onClick={(event) => event.stopPropagation()}
     >
-      <div className="m-5 flex h-96 w-full flex-col gap-4 rounded-xl bg-primary-med p-4 shadow-2xl">
-        <div className="flex justify-between py-2">
-          <h1 className="text-xl font-bold">{title}</h1>
-          <FontAwesomeIcon className="p-2" icon={faX} onClick={onClose} />
-        </div>
-        {children}
+      <div className="relative w-full p-6">
+        <Card>
+          <Card.Header>
+            <h1>{title}</h1>
+            <IconButton icon={faX} noShadow size="xs" onClick={onClose} />
+          </Card.Header>
+          <Card.Body>{children}</Card.Body>
+        </Card>
       </div>
     </div>
   );
