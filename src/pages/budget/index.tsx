e00374214aa6684df3f8075d37ed8b8900fbbef2
Matt Proctor <proctormatt0@gmail.com>
2023-04-27 23:56:54 -0700

    Added modal for creating a budget

--- src/pages/budget/index.tsx
+++ src/pages/budget/index.tsx
@@ -17,13 +17,12 @@ import Budget, {
   BudgetSkeletons,
   BudgetCharts,
 } from "~/features/budgets";
-import { useRouter } from "next/router";
 import Page from "~/components/ui/Page";
 import { useState } from "react";
 import useNotifications from "~/hooks/useNotifications";
+import { CreateBudgetModal } from "~/features/budgets/components/CreateBudgetModal";
 
 export default function Budgets() {
-  const router = useRouter();
   const {
     month,
     year,
@@ -38,83 +37,87 @@ export default function Budgets() {
   );
   const [showSavings, setShowSavings] = useState(true);
   const [showSpending, setShowSpending] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
   const { setErrorNotification } = useNotifications();
 
   return (
-    <Page auth title="Budget">
-      <Header title={`Budget`} subtitle={`${month} ${year}`} />
-      <BudgetCharts data={budgetData.data} />
-      <ButtonBar>
-        {isCurrentMonth && (
-          <Button
-            title="Budget"
-            icon={faPlus}
-            style="secondary"
-            onClick={() => void router.push("/budget/create")}
-          />
-        )}
-        {!isCurrentMonth && (
-          <Button
-            title="Current"
-            icon={faRedo}
-            onClick={setCurrentMonth}
-            style="secondary"
-          />
-        )}
-      </ButtonBar>
-      <MonthYearSelector />
-      {!budgetData.data && <BudgetSkeletons />}
-      {budgetData.data && (
-        <>
-          <IncomeBudget />
-          <div
-            className="flex w-full items-center justify-between"
-            onClick={() => setShowSpending((prev) => !prev)}
-          >
-            <div className="flex gap-1">
-              <IconButton
-                icon={showSpending ? faAngleUp : faAngleDown}
-                noShadow
-              />
-              <h3 className="w-full text-left text-xl font-bold text-primary-light">
-                Spending
-              </h3>
+    <>
+      <Page auth title="Budget">
+        <Header title={`Budget`} subtitle={`${month} ${year}`} />
+        <BudgetCharts data={budgetData.data} />
+        <ButtonBar>
+          {isCurrentMonth && (
+            <Button
+              title="Budget"
+              icon={faPlus}
+              style="secondary"
+              onClick={() => setModalOpen(true)}
+            />
+          )}
+          {!isCurrentMonth && (
+            <Button
+              title="Current"
+              icon={faRedo}
+              onClick={setCurrentMonth}
+              style="secondary"
+            />
+          )}
+        </ButtonBar>
+        <MonthYearSelector />
+        {!budgetData.data && <BudgetSkeletons />}
+        {budgetData.data && (
+          <>
+            <IncomeBudget />
+            <div
+              className="flex w-full items-center justify-between"
+              onClick={() => setShowSpending((prev) => !prev)}
+            >
+              <div className="flex gap-1">
+                <IconButton
+                  icon={showSpending ? faAngleUp : faAngleDown}
+                  noShadow
+                />
+                <h3 className="w-full text-left text-xl font-bold text-primary-light">
+                  Spending
+                </h3>
+              </div>
+              <span className="font-bold text-primary-light">
+                {formatToCurrency(budgetData.data?.spending.total)}
+              </span>
             </div>
-            <span className="font-bold text-primary-light">
-              {formatToCurrency(budgetData.data?.spending.total)}
-            </span>
-          </div>
-          <div className="flex w-full flex-col gap-3">
-            {showSpending &&
-              budgetData.data?.spending.budgets.map((budget) => (
-                <Budget key={budget.id} data={budget} />
-              ))}
-          </div>
-          <div
-            className="flex w-full items-center justify-between"
-            onClick={() => setShowSavings((prev) => !prev)}
-          >
-            <div className="flex gap-1">
-              <IconButton
-                icon={showSavings ? faAngleUp : faAngleDown}
-                noShadow
-              />
-              <h3 className="w-full text-left text-xl font-bold text-primary-light">
-                Savings
-              </h3>
+            <div className="flex w-full flex-col gap-3">
+              {showSpending &&
+                budgetData.data?.spending.budgets.map((budget) => (
+                  <Budget key={budget.id} data={budget} />
+                ))}
             </div>
-            <span className="font-bold text-primary-light">
-              {formatToCurrency(budgetData.data?.savings.total)}
-            </span>
-          </div>
-          <div className="flex w-full flex-col gap-3">
-            {showSavings &&
-              budgetData.data?.savings.budgets.map((budget) => (
-                <SavingsBudget key={budget.id} data={budget} />
-              ))}
-          </div>
-        </>
-      )}
-    </Page>
+            <div
+              className="flex w-full items-center justify-between"
+              onClick={() => setShowSavings((prev) => !prev)}
+            >
+              <div className="flex gap-1">
+                <IconButton
+                  icon={showSavings ? faAngleUp : faAngleDown}
+                  noShadow
+                />
+                <h3 className="w-full text-left text-xl font-bold text-primary-light">
+                  Savings
+                </h3>
+              </div>
+              <span className="font-bold text-primary-light">
+                {formatToCurrency(budgetData.data?.savings.total)}
+              </span>
+            </div>
+            <div className="flex w-full flex-col gap-3">
+              {showSavings &&
+                budgetData.data?.savings.budgets.map((budget) => (
+                  <SavingsBudget key={budget.id} data={budget} />
+                ))}
+            </div>
+          </>
+        )}
+      </Page>
+      <CreateBudgetModal open={modalOpen} onClose={() => setModalOpen(false)} />
+    </>
   );
 }
