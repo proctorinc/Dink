6077ad5c822ea026cc1407f6bcda092511c072cf
Matt Proctor <proctormatt0@gmail.com>
2023-04-27 23:56:41 -0700

    Removed account deletion

--- src/pages/accounts/[accountId]/index.tsx
+++ src/pages/accounts/[accountId]/index.tsx
@@ -1,38 +1,22 @@
-import {
-  faAngleUp,
-  faBuildingColumns,
-  faGear,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBuildingColumns } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 import Transaction from "~/features/transactions";
 import Header from "~/components/ui/Header";
 import Spinner from "~/components/ui/Spinner";
 import { formatToCurrency, formatToTitleCase } from "~/utils";
 import { api } from "~/utils/api";
-import Button, { ButtonBar, IconButton } from "~/components/ui/Button";
-import { useState } from "react";
-import ConfirmDelete from "~/components/ui/ConfirmDelete";
+import { IconButton } from "~/components/ui/Button";
 import Page from "~/components/ui/Page";
 import Image from "next/image";
+import Card from "~/components/ui/Card";
 
 const AccountPage = () => {
   const router = useRouter();
-  const ctx = api.useContext();
-  const [settingsOpen, setSettingsOpen] = useState(false);
   const { accountId } = router.query;
   const strAccountId = typeof accountId === "string" ? accountId : null;
   const accountData = api.bankAccounts.getById.useQuery({
     accountId: strAccountId ?? "",
   });
-  const deleteAccount = api.bankAccounts.delete.useMutation({
-    onSuccess: () => void ctx.invalidate(),
-  });
-
-  const handleDeleteAccount = () => {
-    deleteAccount.mutate({ accountId: accountData?.data?.id ?? "" });
-    void router.push("/accounts");
-  };
-
   const account = accountData.data;
 
   if (accountData.isLoading) {
@@ -80,33 +64,19 @@ const AccountPage = () => {
             <h3>{accountData?.data?.name}</h3>
           </div>
         }
-        subtitle={`Total: ${formatToCurrency(accountData?.data?.current)}`}
+        subtitle={`Balance: ${formatToCurrency(accountData?.data?.current)}`}
       />
-      <div className="flex w-full flex-col gap-2">
-        {account?.officialName && (
-          <span className="flex h-fit w-fit items-center rounded-lg bg-primary-med px-2 py-1 text-xs font-bold text-primary-light">
-            {account.officialName}
-          </span>
-        )}
-        <span className="text-md text-primary-light">
-          {formatToTitleCase(accountData?.data?.type)} /{" "}
-          {formatToTitleCase(accountData?.data?.subtype)} / {account?.mask}
-        </span>
-      </div>
-      <ButtonBar>
-        <Button
-          icon={settingsOpen ? faAngleUp : faGear}
-          onClick={() => setSettingsOpen((prev) => !prev)}
-        />
-      </ButtonBar>
-      {settingsOpen && (
-        <ButtonBar>
-          <ConfirmDelete
-            confirmationText={accountData?.data?.name ?? "delete account"}
-            onDelete={handleDeleteAccount}
-          />
-        </ButtonBar>
-      )}
+      <Card size="sm">
+        <Card.Body>
+          <Card.Group size="sm" className="text-center text-primary-light">
+            <span>{account?.officialName}</span>
+            <span className="text-sm text-primary-light">
+              {formatToTitleCase(accountData?.data?.type)} /{" "}
+              {formatToTitleCase(accountData?.data?.subtype)} / {account?.mask}
+            </span>
+          </Card.Group>
+        </Card.Body>
+      </Card>
       <div className="w-full">
         <h2 className="text-left text-xl text-primary-light">Transactions</h2>
       </div>
