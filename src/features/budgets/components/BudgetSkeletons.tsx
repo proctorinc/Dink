e7bab039ea9508eaa5bfbf40e765db2a6f3a18da
Matt Proctor <proctormatt0@gmail.com>
2023-04-27 23:54:52 -0700

    Fixed to match real budgets

--- src/features/budgets/components/BudgetSkeletons.tsx
+++ src/features/budgets/components/BudgetSkeletons.tsx
@@ -10,48 +10,50 @@ import { ProgressBar } from "~/components/ui/Charts";
 import { TextSkeleton } from "~/components/ui/Skeleton";
 import { IncomeBudgetSkeleton } from "./IncomeBudgetSkeleton";
 
-export const BudgetSkeletons = () => {
-  const BudgetSkeleton = () => (
-    <CardSkeleton>
-      <Card.Body horizontal>
-        <IconButton
-          className="bg-secondary-dark/50 text-secondary-med/50"
-          icon={faCalendarAlt}
-          style="secondary"
-        />
-        <Card.Group size="sm" className="w-full gap-1 pl-2">
-          <TextSkeleton width={100} />
+const BudgetSkeleton = () => (
+  <CardSkeleton>
+    <Card.Body size="sm" horizontal>
+      <IconButton
+        className="bg-secondary-dark/50 text-secondary-med/50"
+        icon={faCalendarAlt}
+        style="secondary"
+      />
+      <Card.Group size="sm" className="w-full gap-1 pl-2">
+        <TextSkeleton width={100} />
+        <div className="py-0.5">
           <ProgressBar
             size="sm"
             value={new Prisma.Decimal(60)}
             goal={new Prisma.Decimal(100)}
             className="bg-gradient-to-r from-secondary-dark/75 to-secondary-med/75"
           />
-          <div className="flex justify-between text-sm text-primary-light group-hover:text-primary-med">
-            <TextSkeleton size="sm" color="primary" width={125} />
-            <TextSkeleton size="sm" color="primary" width={100} />
-          </div>
-        </Card.Group>
-      </Card.Body>
-    </CardSkeleton>
-  );
+        </div>
+        <div className="flex justify-between text-sm text-primary-light group-hover:text-primary-med">
+          <TextSkeleton size="sm" color="primary" width={125} />
+          <TextSkeleton size="sm" color="primary" width={100} />
+        </div>
+      </Card.Group>
+    </Card.Body>
+  </CardSkeleton>
+);
 
-  const SavingsBudgetSkeleton = () => (
-    <CardSkeleton>
-      <Card.Body horizontal>
-        <Card.Group size="sm">
-          <Card.Group horizontal>
-            <IconButton icon={faSackDollar} size="sm" style="secondary" />
-            <TextSkeleton width={125} color="primary" />
-          </Card.Group>
+const SavingsBudgetSkeleton = () => (
+  <CardSkeleton>
+    <Card.Body horizontal>
+      <Card.Group size="sm">
+        <Card.Group horizontal>
+          <IconButton icon={faSackDollar} size="sm" style="secondary" />
+          <TextSkeleton width={125} color="primary" />
         </Card.Group>
-        <Card.Group horizontal size="sm">
-          <TextSkeleton color="primary" width={125} />
-        </Card.Group>
-      </Card.Body>
-    </CardSkeleton>
-  );
+      </Card.Group>
+      <Card.Group horizontal size="sm">
+        <TextSkeleton color="primary" width={125} />
+      </Card.Group>
+    </Card.Body>
+  </CardSkeleton>
+);
 
+export const BudgetSkeletons = () => {
   return (
     <>
       <IncomeBudgetSkeleton />
