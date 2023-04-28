4b5bf4ca53ae9ae2566a08d87339cb8c19dfe39c
Matt Proctor <proctormatt0@gmail.com>
2023-04-27 23:51:59 -0700

    Created modal using headless ui dialog

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,30 +1,37 @@
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faSquareXmark } from "@fortawesome/free-solid-svg-icons";
+import { Dialog } from "@headlessui/react";
 import { type FC, type ReactNode } from "react";
 import { IconButton } from "./Button";
-import Card from "./Card";
 
 type ModalProps = {
+  open: boolean;
   children: ReactNode;
   title: string;
   onClose: () => void;
 };
 
-const Modal: FC<ModalProps> = ({ children, title, onClose }) => {
+const Modal: FC<ModalProps> = ({ open, children, title, onClose }) => {
   return (
-    <div
-      className="fixed inset-0 z-50 flex h-screen w-full items-center justify-center overflow-x-hidden overflow-y-hidden overscroll-auto bg-primary-dark/60 text-white shadow-2xl backdrop-blur-sm"
-      onClick={(event) => event.stopPropagation()}
-    >
-      <div className="relative w-full p-6">
-        <Card>
-          <Card.Header>
-            <h1>{title}</h1>
-            <IconButton icon={faX} noShadow size="xs" onClick={onClose} />
-          </Card.Header>
-          <Card.Body>{children}</Card.Body>
-        </Card>
+    <Dialog as="div" className="relative z-50" open={open} onClose={onClose}>
+      <div className="fixed inset-0 overflow-y-auto text-white">
+        <div className="flex min-h-full items-center justify-center bg-primary-dark/60 p-4 text-center backdrop-blur-sm">
+          <Dialog.Panel className="w-full max-w-lg rounded-xl bg-primary-med shadow-xl shadow-primary-dark">
+            <div className="flex justify-between px-4 pt-4">
+              <Dialog.Title className="text-2xl font-bold">
+                {title}
+              </Dialog.Title>
+              <IconButton
+                icon={faSquareXmark}
+                noShadow
+                size="sm"
+                onClick={onClose}
+              />
+            </div>
+            <div className="flex flex-col gap-4 p-4 text-left">{children}</div>
+          </Dialog.Panel>
+        </div>
       </div>
-    </div>
+    </Dialog>
   );
 };
 
