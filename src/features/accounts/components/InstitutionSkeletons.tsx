8935b371037d0f9ed4af56b62f4cf60c0c772fcf
Matt Proctor <proctormatt0@gmail.com>
2023-04-27 23:54:34 -0700

    Fixed to match real institutions

--- src/features/accounts/components/InstitutionSkeletons.tsx
+++ src/features/accounts/components/InstitutionSkeletons.tsx
@@ -6,21 +6,18 @@ import { TextSkeleton } from "~/components/ui/Skeleton";
 export const InstitutionSkeletons = () => {
   const InstitutionSkeleton = () => (
     <CardSkeleton>
-      <Card.Header>
+      <Card.Header size="xl">
         <Card.Group horizontal>
           <div className="flex aspect-square h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-primary-dark shadow-xl">
             <IconButton icon={faBuildingColumns} />
           </div>
           <Card.Group size="sm">
-            <TextSkeleton width={80} />
-            <TextSkeleton width={125} color="primary" />
+            <TextSkeleton width={90} size="xl" />
+            <TextSkeleton width={125} size="sm" color="primary" />
           </Card.Group>
         </Card.Group>
         <IconButton icon={faGear} noShadow size="sm" />
       </Card.Header>
-      <Card.Body>
-        <TextSkeleton width={150} color="primary" />
-      </Card.Body>
     </CardSkeleton>
   );
 
