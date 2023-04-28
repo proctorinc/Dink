0855cd35bc81001a5add6693603aa181c40810df
Matt Proctor <proctormatt0@gmail.com>
2023-04-27 23:52:23 -0700

    Updated progress colors

--- src/components/ui/Charts/ProgressBar.tsx
+++ src/components/ui/Charts/ProgressBar.tsx
@@ -1,6 +1,5 @@
 import { type Prisma } from "@prisma/client";
 import { type FC } from "react";
-import { formatToProgressPercentage } from "~/utils";
 
 type ProgressBarProps = {
   value?: Prisma.Decimal;
@@ -15,19 +14,33 @@ export const ProgressBar: FC<ProgressBarProps> = ({
   size,
   className,
 }) => {
-  const percentComplete = formatToProgressPercentage(value, goal);
+  const percentComplete = (Number(value) / Number(goal)) * 100;
+  let formattedPercent = percentComplete;
+  let barColor = "from-secondary-dark to-secondary-med";
 
   const barHeight = size === "sm" ? "h-4" : "h-6";
+
+  if (percentComplete > 100) {
+    formattedPercent = 100;
+    barColor = "from-danger-dark to-danger-med";
+  }
+  if (percentComplete > 90 && percentComplete < 100) {
+    barColor = "from-warning-dark to-warning-med";
+  }
+
+  if (percentComplete < 0) {
+    formattedPercent = 0;
+  }
 
   return (
     <div
       className={`${barHeight} relative w-full rounded-md bg-primary-dark shadow-inner group-hover:bg-primary-med`}
     >
       <div
-        className={`absolute h-full rounded-md bg-gradient-to-r from-secondary-dark to-secondary-med ${
+        className={`${barColor} absolute h-full rounded-md bg-gradient-to-r ${
           className ?? ""
         }`}
-        style={{ width: percentComplete }}
+        style={{ width: `${formattedPercent}%` }}
       ></div>
     </div>
   );
