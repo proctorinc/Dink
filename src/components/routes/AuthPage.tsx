9f215abaf6f2a54b0e784c9f4b4f032e5ffbe2e0
Matt Proctor <proctormatt0@gmail.com>
2023-06-14 23:21:41 -0700

    Added auth loading

--- src/components/routes/AuthPage.tsx
+++ src/components/routes/AuthPage.tsx
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { type FC, type ReactNode } from "react";
+import LoadingPage from "../ui/LoadingPage";
 
 type AuthPageProps = {
   children: ReactNode;
@@ -19,7 +20,11 @@ const AuthPage: FC<AuthPageProps> = ({ children }) => {
     });
   }
 
-  return <>{children}</>;
+  if (typeof window !== "undefined" && status === "authenticated") {
+    return <>{children}</>;
+  }
+
+  return <LoadingPage />;
 };
 
 export default AuthPage;
