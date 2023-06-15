e157d686a4edb45b77a1aacfbe42c86fa618d700
Matt Proctor <proctormatt0@gmail.com>
2023-06-14 23:26:02 -0700

    Added setProfileComplete mutation

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -2,6 +2,16 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
+  setProfileComplete: protectedProcedure.mutation(({ ctx }) => {
+    return ctx.prisma.user.update({
+      where: {
+        id: ctx.session.user.id,
+      },
+      data: {
+        isProfileComplete: true,
+      },
+    });
+  }),
   getUserPreferences: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.userPreferences.findUnique({
       where: {
@@ -30,9 +40,18 @@ export const userRouter = createTRPCRouter({
         },
       });
     }),
-  updateTargetIncome: protectedProcedure
-    .input(z.object({ income: z.number() }))
+  completeProfile: protectedProcedure
+    .input(z.object({ income: z.number(), nickname: z.string() }))
     .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          nickname: input.nickname,
+          isProfileComplete: true,
+        },
+      });
       return ctx.prisma.userPreferences.upsert({
         where: {
           userId: ctx.session.user.id,
