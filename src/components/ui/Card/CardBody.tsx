a8d62f3ca351a49b49b2e3240119f0c66159a798
Matt Proctor <proctormatt0@gmail.com>
2023-06-14 23:22:58 -0700

    Added custom styling

--- src/components/ui/Card/CardBody.tsx
+++ src/components/ui/Card/CardBody.tsx
@@ -4,16 +4,26 @@ export type CardBodyProps = {
   children: ReactNode;
   size?: "sm";
   horizontal?: boolean;
+  className?: string;
 };
 
-const CardBody: FC<CardBodyProps> = ({ size, horizontal, children }) => {
+const CardBody: FC<CardBodyProps> = ({
+  size,
+  horizontal,
+  className,
+  children,
+}) => {
   const verticalPadding = size === "sm" ? "py-2" : "py-4";
   const orientation = horizontal
     ? "items-center justify-between"
     : "flex-col justify-center";
 
   return (
-    <div className={`${orientation} ${verticalPadding} flex w-full gap-1 px-4`}>
+    <div
+      className={`${orientation} ${verticalPadding} flex w-full gap-1 px-4 ${
+        className ?? ""
+      }`}
+    >
       {children}
     </div>
   );
