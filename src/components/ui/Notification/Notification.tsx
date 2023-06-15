000cd454d6c71ab8d0b6bca54dbac1f69bfb7323
Matt Proctor <proctormatt0@gmail.com>
2023-06-14 23:23:32 -0700

    Fixed styling

--- src/components/ui/Notification/Notification.tsx
+++ src/components/ui/Notification/Notification.tsx
@@ -17,17 +17,28 @@ export type NotificationProps = {
 export const Notification: FC<NotificationProps> = ({ type, message }) => {
   const { clearNotification } = useNotifications();
 
+  let styling = "bg-primary-dark text-sm text-primary-light shadow-none";
+
+  if (type === "info") {
+    styling = "bg-secondary-dark text-sm font-semibold text-secondary-med";
+  } else if (type === "error") {
+    styling = "bg-danger-dark text-danger-med";
+  }
+
   return (
     <div
-      className={`${
-        type === "error"
-          ? "bg-danger-dark text-danger-med"
-          : "text-sm text-primary-light"
-      } relative flex w-full items-center justify-center gap-2 p-1 font-bold`}
+      className={`relative flex w-full items-center justify-center gap-2 rounded-xl p-2 pl-12 text-center font-bold shadow-md ${styling}`}
     >
-      {type === "loading" && <Spinner size="xs" />}
-      {type === "error" && <FontAwesomeIcon icon={faExclamationTriangle} />}
-      {type === "info" && <FontAwesomeIcon icon={faInfo} />}
+      <div className="absolute left-5">
+        {type === "loading" && <Spinner size="xs" />}
+        {type === "error" && <FontAwesomeIcon icon={faExclamationTriangle} />}
+        {type === "info" && (
+          <FontAwesomeIcon
+            icon={faInfo}
+            className="h-3 rounded-full border-2 border-secondary-med px-2 py-1"
+          />
+        )}
+      </div>
       <p>{message}</p>
       {type === "error" && (
         <div className="absolute right-3">
