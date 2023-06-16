ce0ad5e475675d6567ec0c9e2e6e14c3fac8b361
Matt Proctor <proctormatt0@gmail.com>
2023-06-16 13:29:18 -0700

    Fixed conditions for showing setup

--- src/pages/profile/setup/index.tsx
+++ src/pages/profile/setup/index.tsx
@@ -38,81 +38,81 @@ export default function ProfileSetup() {
     void router.push("/");
   }
 
-  if (status === "loading") {
-    return <LoadingPage />;
+  if (status === "authenticated" && !sessionData?.user.isProfileComplete) {
+    return (
+      <Page auth title="Get Started" style="centered">
+        <Header title="Welcome!" subtitle="Let's get started" />
+        <div className="flex w-full flex-col gap-4 pt-10">
+          {!nameComplete && (
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={() => setNameComplete(true)}
+            >
+              <h3 className="text-xl font-bold">Enter your name:</h3>
+              <div className="relative w-full">
+                <input
+                  autoFocus
+                  id="name-input"
+                  placeholder="Enter your name..."
+                  className="relative w-full rounded-lg bg-primary-med py-2 pr-4 pl-10 font-bold text-primary-light placeholder-primary-light ring ring-primary-med focus:placeholder-primary-med"
+                  value={nickname}
+                  onChange={(event) => setNickname(event.target.value)}
+                />
+                <IconButton
+                  className="absolute top-0 h-full"
+                  icon={faUserAlt}
+                  noShadow
+                />
+              </div>
+              <Button
+                title="Next"
+                style="secondary"
+                icon={faArrowRight}
+                iconRight
+                type="submit"
+              />
+            </form>
+          )}
+          {nameComplete && (
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={() => {
+                completeProfile.mutate({ income, nickname });
+                setLoadingNotification("Updating profile...");
+              }}
+            >
+              <h3 className="text-xl font-bold">Enter your monthly income:</h3>
+              <div className="relative w-full">
+                <input
+                  autoFocus
+                  id="income-input"
+                  type="number"
+                  className="relative w-full rounded-lg bg-primary-med py-2 pr-4 pl-10 font-bold text-primary-light placeholder-primary-light ring ring-primary-med focus:placeholder-primary-med"
+                  value={income}
+                  onChange={(event) => setIncome(Number(event.target.value))}
+                />
+                <IconButton
+                  className="absolute top-0 h-full"
+                  icon={faDollarSign}
+                  noShadow
+                  disabled
+                />
+              </div>
+              <Button
+                title="Next"
+                style="secondary"
+                icon={faArrowRight}
+                iconRight
+                type="submit"
+              />
+            </form>
+          )}
+        </div>
+      </Page>
+    );
   }
 
-  return (
-    <Page auth title="Get Started" style="centered">
-      <Header title="Welcome!" subtitle="Let's get started" />
-      <div className="flex w-full flex-col gap-4 pt-10">
-        {!nameComplete && (
-          <form
-            className="flex flex-col gap-4"
-            onSubmit={() => setNameComplete(true)}
-          >
-            <h3 className="text-xl font-bold">Enter your name:</h3>
-            <div className="relative w-full">
-              <input
-                autoFocus
-                id="name-input"
-                placeholder="Enter your name..."
-                className="relative w-full rounded-lg bg-primary-med py-2 pr-4 pl-10 font-bold text-primary-light placeholder-primary-light ring ring-primary-med focus:placeholder-primary-med"
-                value={nickname}
-                onChange={(event) => setNickname(event.target.value)}
-              />
-              <IconButton
-                className="absolute top-0 h-full"
-                icon={faUserAlt}
-                noShadow
-              />
-            </div>
-            <Button
-              title="Next"
-              style="secondary"
-              icon={faArrowRight}
-              iconRight
-              type="submit"
-            />
-          </form>
-        )}
-        {nameComplete && (
-          <form
-            className="flex flex-col gap-4"
-            onSubmit={() => {
-              completeProfile.mutate({ income, nickname });
-              setLoadingNotification("Updating profile...");
-            }}
-          >
-            <h3 className="text-xl font-bold">Enter your monthly income:</h3>
-            <div className="relative w-full">
-              <input
-                autoFocus
-                id="income-input"
-                type="number"
-                className="relative w-full rounded-lg bg-primary-med py-2 pr-4 pl-10 font-bold text-primary-light placeholder-primary-light ring ring-primary-med focus:placeholder-primary-med"
-                value={income}
-                onChange={(event) => setIncome(Number(event.target.value))}
-              />
-              <IconButton
-                className="absolute top-0 h-full"
-                icon={faDollarSign}
-                noShadow
-                disabled
-              />
-            </div>
-            <Button
-              title="Next"
-              style="secondary"
-              icon={faArrowRight}
-              iconRight
-              type="submit"
-            />
-          </form>
-        )}
-      </div>
-    </Page>
-  );
+  return <LoadingPage />;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
