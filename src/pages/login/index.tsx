9341674e1d043e58c95c9469196a35c64184bdff
Matt Proctor <proctormatt0@gmail.com>
2023-06-22 15:28:13 -0700

    Updated login UI with background SVG

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,18 @@
-import { faEnvelope, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRight,
+  faCircleHalfStroke,
+  faEnvelope,
+  faPaperPlane,
+  faSquareXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { type GetServerSideProps } from "next";
 import { getSession, signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { z } from "zod";
-import Button from "~/components/ui/Button";
-import Header from "~/components/ui/Header";
+import Button, { IconButton } from "~/components/ui/Button";
+import Card from "~/components/ui/Card";
 import LoadingPage from "~/components/ui/LoadingPage";
 import Page from "~/components/ui/Page";
 import useNotifications from "~/hooks/useNotifications";
@@ -55,53 +62,83 @@ const Login = () => {
   if (status === "unauthenticated") {
     return (
       <Page title="Login" style="centered">
-        <div className="w-full max-w-sm px-4">
-          <Header title="Login" />
-        </div>
-        <div className="flex w-full max-w-sm flex-col gap-4 px-4 pt-10">
-          <Button
-            title="Try the Demo"
-            className="w-full"
-            style={open ? "primary" : "secondary"}
-            icon={faPaperPlane}
-            onClick={() => void signIn("google")}
-          />
-          <div className="flex items-center justify-center gap-3 py-2">
-            <div className="h-0.5 w-full rounded-full bg-primary-med" />
-            <span className="text-xs font-bold text-primary-light">OR</span>
-            <div className="h-0.5 w-full rounded-full bg-primary-med" />
-          </div>
-          {!open && (
-            <Button
-              title="Request Access"
-              className="w-full"
-              style="primary"
-              icon={faEnvelope}
-              onClick={() => setOpen(true)}
-            />
-          )}
-          {open && (
-            <form
-              onSubmit={requestAccess}
-              className="flex w-full flex-col gap-4"
-            >
-              <input
-                autoFocus
-                placeholder="Enter Email..."
-                className="rounded-xl bg-primary-med py-2 px-5 text-lg font-bold text-primary-light placeholder-primary-light"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
-              <Button
-                title="Request Access"
-                className="w-full"
-                style="secondary"
-                size="lg"
-                icon={faEnvelope}
-                type="submit"
-              />
-            </form>
-          )}
+        <div
+          className="absolute top-0 z-0 h-screen w-full"
+          style={{
+            backgroundImage: `url(/static/images/stacked-waves-horizontal-light.svg)`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="absolute top-0 flex h-3/4 w-full items-center justify-center p-4 sm:h-screen">
+          <Card className="z-10 flex w-full max-w-sm flex-col items-center from-primary-med-dark to-primary-dark shadow-2xl">
+            <Card.Body className="items-center">
+              <div className="flex items-center gap-2 rounded-xl py-1 px-3 text-5xl font-bold text-primary-light">
+                <FontAwesomeIcon
+                  className="h-10 w-10 text-primary-light"
+                  icon={faCircleHalfStroke}
+                />
+                <span>Dink</span>
+              </div>
+              <div className="flex w-full max-w-sm flex-col gap-4 px-4 pt-10">
+                <Button
+                  title="Try the Demo"
+                  className="w-full"
+                  style={open ? "primary" : "secondary"}
+                  icon={faPaperPlane}
+                  onClick={() => void signIn("google")}
+                />
+                <div className="flex items-center justify-center gap-3 py-2">
+                  <div className="h-0.5 w-full rounded-full bg-primary-med" />
+                  <span className="text-xs font-bold text-primary-light">
+                    OR
+                  </span>
+                  <div className="h-0.5 w-full rounded-full bg-primary-med" />
+                </div>
+                {!open && (
+                  <Button
+                    title="Request Access"
+                    className="w-full"
+                    style="primary"
+                    icon={faEnvelope}
+                    onClick={() => setOpen(true)}
+                  />
+                )}
+                {open && (
+                  <form
+                    onSubmit={requestAccess}
+                    className="flex w-full flex-col gap-4"
+                  >
+                    <div className="flex items-center gap-2">
+                      <input
+                        autoFocus
+                        placeholder="Enter Email..."
+                        className="w-full rounded-xl bg-primary-med py-2 px-5 text-lg font-bold text-primary-light placeholder-primary-light"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                      />
+                      <IconButton
+                        size="sm"
+                        icon={faSquareXmark}
+                        onClick={() => {
+                          setOpen(false);
+                          setEmail("");
+                        }}
+                      />
+                    </div>
+                    <Button
+                      title={open ? "Submit" : "Request Access"}
+                      className="w-full"
+                      style="secondary"
+                      size="lg"
+                      icon={open ? faArrowRight : faEnvelope}
+                      type="submit"
+                      iconRight={open}
+                    />
+                  </form>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
         </div>
       </Page>
     );
