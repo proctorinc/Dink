c944e1f2c3f69a05e245e6947c6f48d2f59733ba
Matt Proctor <proctormatt0@gmail.com>
2023-12-19 22:56:53 -0800

    Removed savings charts

--- src/features/funds/index.ts
+++ src/features/funds/index.ts
@@ -2,7 +2,6 @@ import Fund from "./components/Fund";
 
 export * from "./components/SavingsSummary";
 export * from "./components/SavingsSummarySkeleton";
-export * from "./components/SavingsCharts";
 export * from "./components/FundSkeleton";
 export * from "./components/FundBrief";
 export * from "./components/AllocateSavingsDrawer";
