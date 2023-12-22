5599a350e5b812c656d6ef3f355a64cf9eb87562
Matt Proctor <proctormatt0@gmail.com>
2023-12-21 21:32:39 -0800

    Removed transaction categorization components

--- src/features/transactions/index.ts
+++ src/features/transactions/index.ts
@@ -5,7 +5,5 @@ export * from "./components/TransactionsSummary";
 export * from "./components/TransactionSummarySkeleton";
 export * from "./components/DetailedTransaction";
 export * from "./components/TransactionSkeletons";
-export * from "./components/CategorizeTransactions";
-export * from "./components/CategorizeTransactionsDrawer";
 
 export default Transaction;
