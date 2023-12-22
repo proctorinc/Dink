d5234fe4da8b75ee3278ea1f97acb308d29db349
Matt Proctor <proctormatt0@gmail.com>
2023-12-21 21:09:03 -0800

    Added custom colors and removed patterns

--- src/components/ui/Charts/PieChart.tsx
+++ src/components/ui/Charts/PieChart.tsx
@@ -7,65 +7,24 @@ type PieChartProps = {
   data: unknown[];
   progress?: boolean;
   floatRight?: boolean;
+  colors?: string[];
 };
 
-export const PieChart: FC<PieChartProps> = ({ data, progress, floatRight }) => {
+export const PieChart: FC<PieChartProps> = ({
+  data,
+  colors,
+  progress,
+  floatRight,
+}) => {
   const theme = {
     fontSize: 15,
   };
-
-  const patterns = [
-    {
-      id: "dots",
-      type: "patternDots",
-      background: "#00C6C5",
-      color: "rgb(22, 123, 141, 0.1)",
-      size: 6,
-      padding: 4,
-      stagger: true,
-    },
-    {
-      id: "lines",
-      type: "patternLines",
-      background: "#167B8D",
-      color: "rgba(255, 255, 255, 0.05)",
-      rotation: -45,
-      lineWidth: 6,
-      spacing: 10,
-    },
-    {
-      id: "gradient",
-      type: "linearGradient",
-      colors: [
-        { offset: 0, color: "#00C6C5" },
-        { offset: 100, color: "#126473" },
-      ],
-    },
-  ];
 
   return (
     <ResponsivePie
       theme={theme}
       data={data}
-      colors={progress ? ["#00C6C5", "#2A285B"] : []}
-      defs={patterns}
-      fill={
-        progress
-          ? [
-              {
-                match: (d) => d.arc.index % 2 === 0,
-                id: "gradient",
-              },
-              { match: "*", id: "gradientPrimary" },
-            ]
-          : [
-              {
-                match: (d) => d.arc.angle < 0.2 || d.arc.index % 2 === 1,
-                id: "lines",
-              },
-              { match: "*", id: "dots" },
-            ]
-      }
+      colors={colors ?? []}
       id="name"
       value="amount"
       margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
@@ -74,7 +33,7 @@ export const PieChart: FC<PieChartProps> = ({ data, progress, floatRight }) => {
       padAngle={2}
       cornerRadius={progress ? 2 : 5}
       activeOuterRadiusOffset={10}
-      sortByValue={progress ? false : true}
+      sortByValue={false}
       enableArcLinkLabels={false}
       arcLabelsSkipAngle={15}
       enableArcLabels={true}
@@ -87,11 +46,12 @@ export const PieChart: FC<PieChartProps> = ({ data, progress, floatRight }) => {
         <div
           className={`${
             floatRight ? "translate-x-16" : "-translate-x-12"
-          } flex w-fit -translate-y-2 items-center gap-2 rounded-xl bg-primary-med px-3 py-1 font-bold shadow-2xl`}
+          } z-50 flex w-fit translate-y-8 items-center gap-2 rounded-xl bg-primary-med px-3 py-1 font-bold text-white shadow-2xl`}
+          style={{ backgroundColor: data.color }}
         >
           <div className="flex flex-col">
             <span>{data.id}</span>
-            <span className="text-primary-light">{data.formattedValue}</span>
+            <span className="font-normal">{data.formattedValue}</span>
           </div>
         </div>
       )}
