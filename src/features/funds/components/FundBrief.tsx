836d80a835c237e11c3f9944c8ce83a2a7d7a4b1
Matt Proctor <proctormatt0@gmail.com>
2023-12-21 21:17:30 -0800

    Updated fund brief UI

--- src/features/funds/components/FundBrief.tsx
+++ src/features/funds/components/FundBrief.tsx
@@ -5,16 +5,25 @@ import { faPiggyBank, faRetweet } from "@fortawesome/free-solid-svg-icons";
 import { formatToTitleCase } from "~/utils";
 import useIcons from "~/hooks/useIcons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconColor } from "~/config";
 
 type FundProps = HTMLAttributes<HTMLDivElement> & {
   data?: Fund & {
     amount: Prisma.Decimal;
   };
+  onClick?: () => void;
+  className?: string;
 };
 
-const FundBrief: FC<FundProps> = ({ data: fund, className, ...otherProps }) => {
-  const { convertToIcon } = useIcons();
+const FundBrief: FC<FundProps> = ({
+  data: fund,
+  className,
+  onClick,
+  ...otherProps
+}) => {
+  const { convertToIcon, convertToColor, defaultColor } = useIcons();
   const icon = convertToIcon(fund?.icon) ?? faPiggyBank;
+  const color: IconColor = convertToColor(fund?.color) ?? defaultColor;
 
   return (
     <div
@@ -22,13 +31,19 @@ const FundBrief: FC<FundProps> = ({ data: fund, className, ...otherProps }) => {
         className ?? ""
       }`}
       {...otherProps}
-      onClick={() => console.log("ook")}
+      onClick={onClick ? () => onClick() : () => null}
     >
       <div className="flex w-full items-center justify-between">
         <div className="flex items-center gap-2">
-          <div className="text-gray-500">
-            <IconButton style="secondary" size="sm" icon={icon} />
-          </div>
+          <button
+            className="h-8 w-8 rounded-lg shadow-md"
+            style={{
+              backgroundColor: color?.primary,
+              color: color?.secondary,
+            }}
+          >
+            <FontAwesomeIcon size="lg" icon={icon} />
+          </button>
           <h3>{formatToTitleCase(fund?.name)}</h3>
         </div>
         <FontAwesomeIcon icon={faRetweet} className="text-gray-600" />
