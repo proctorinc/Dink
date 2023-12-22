8cb849782eb727b291e5b108fb541101a99689cd
Matt Proctor <proctormatt0@gmail.com>
2023-12-21 21:09:22 -0800

    Added support for intial icon and color

--- src/components/ui/Icons/IconPickerModal.tsx
+++ src/components/ui/Icons/IconPickerModal.tsx
@@ -1,62 +1,106 @@
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleCheck,
+  faPlusCircle,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState, type FC } from "react";
 import Modal from "~/components/ui/Modal";
 import { type IconColor } from "~/config";
 import useIcons from "~/hooks/useIcons";
+import Button from "../Button";
 
 type FundPickerModalProps = {
   open: boolean;
   onSelect: (iconName: string, iconColor: IconColor) => void;
+  initialIcon?: string;
+  initialColor?: string;
   onClose: () => void;
 };
 
 const IconPickerModal: FC<FundPickerModalProps> = ({
   open,
+  initialIcon,
+  initialColor,
   onSelect,
   onClose,
 }) => {
-  const { icons, colors, defaultColor } = useIcons();
-  const [selectedColor, setSelectedColor] = useState<IconColor>(defaultColor);
+  const { icons, colors, convertToIcon, convertToColor, defaultColor } =
+    useIcons();
+  const [selectedColor, setSelectedColor] = useState<IconColor>(
+    convertToColor(initialColor) ?? defaultColor
+  );
+  const [selectedIcon, setSelectedIcon] = useState<string | null>(
+    initialIcon ?? null
+  );
+
+  const isValidData =
+    selectedIcon !== null &&
+    (selectedColor.name !== initialColor || selectedIcon !== initialIcon);
+
+  const submitForm = () => {
+    if (isValidData) {
+      onSelect(selectedIcon, selectedColor);
+    }
+  };
 
   return (
     <Modal title="Choose Icon" open={open} onClose={onClose}>
-      <label className="px-2">Color:</label>
-      <div className="grid grid-cols-6 gap-2 text-black">
-        {colors.map((color) => (
-          <div
-            key={color.name}
-            className="flex aspect-square w-full items-center justify-center rounded-xl shadow-md"
+      <form onSubmit={submitForm} className="flex flex-col gap-4">
+        <div className="flex w-full justify-center p-4">
+          <button
+            className="h-20 w-20 rounded-xl shadow-md"
             style={{
-              backgroundColor: color.primary,
-              color: color.secondary,
+              backgroundColor: selectedColor.primary,
+              color: selectedColor.secondary,
             }}
-            onClick={() => setSelectedColor(color)}
           >
-            {color.name === selectedColor?.name && (
-              <FontAwesomeIcon icon={faCircleCheck} size="xl" />
-            )}
-          </div>
-        ))}
-      </div>
-      <label className="px-2">Icon:</label>
-      <div
-        style={{ backgroundColor: selectedColor?.primary }}
-        className="grid max-h-64 grid-cols-5 justify-center gap-2 overflow-y-scroll rounded-xl border border-gray-300 bg-gray-100"
-      >
-        {icons.map((iconChoice) => (
-          <FontAwesomeIcon
-            key={iconChoice.name}
-            icon={iconChoice.icon}
-            size="lg"
-            className="aspect-square w-8 rounded-xl p-4"
-            style={{
-              color: selectedColor?.secondary,
-            }}
-            onClick={() => onSelect(iconChoice.name, selectedColor)}
+            <FontAwesomeIcon
+              size="3x"
+              icon={convertToIcon(selectedIcon) ?? faPlusCircle}
+            />
+          </button>
+        </div>
+        <div className="grid grid-cols-6 gap-2 text-black">
+          {colors.slice(0, -1).map((color) => (
+            <div
+              key={color.name}
+              className="flex aspect-square w-full items-center justify-center rounded-xl shadow-md"
+              style={{
+                backgroundColor: color.primary,
+                color: color.secondary,
+              }}
+              onClick={() => setSelectedColor(color)}
+            >
+              {color.name === selectedColor?.name && (
+                <FontAwesomeIcon icon={faCircleCheck} size="xl" />
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="grid max-h-64 grid-cols-5 justify-center gap-2 overflow-y-scroll rounded-xl border border-gray-300 bg-gray-100 text-gray-500">
+          {icons.map((iconChoice) => (
+            <FontAwesomeIcon
+              key={iconChoice.name}
+              icon={iconChoice.icon}
+              size="xl"
+              className={`aspect-square w-8 rounded-xl py-4 px-3 ${
+                selectedIcon === iconChoice.name ? "bg-gray-600 text-white" : ""
+              }`}
+              onClick={() => setSelectedIcon(iconChoice.name)}
+            />
+          ))}
+        </div>
+        <div className="mt-5 flex w-full justify-center">
+          <Button
+            style="secondary"
+            type="submit"
+            title="Choose"
+            className="w-full"
+            disabled={!isValidData}
           />
-        ))}
-      </div>
+        </div>
+      </form>
     </Modal>
   );
 };
