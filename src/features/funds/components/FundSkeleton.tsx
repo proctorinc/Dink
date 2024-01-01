aa36d78eb6c4c8b658d322b79bd7c51f5d5a8988
Matt Proctor <proctormatt0@gmail.com>
2023-12-31 16:37:18 -0800

    Improved fund skeleton

--- src/features/funds/components/FundSkeleton.tsx
+++ src/features/funds/components/FundSkeleton.tsx
@@ -7,7 +7,7 @@ export const FundSkeletons = () => {
     <div className="flex w-full animate-pulse items-center border-b border-gray-300 bg-white/50 p-4">
       <div className="flex w-full items-center justify-between">
         <div className="flex items-center gap-2">
-          <button className="h-8 w-8 rounded-lg shadow-md">
+          <button className="h-8 w-8 rounded-lg bg-secondary-dark text-secondary-med shadow-md">
             <FontAwesomeIcon size="lg" icon={faPiggyBank} />
           </button>
           <TextSkeleton maxWidth={125} minWidth={50} />
