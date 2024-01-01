7039a9d88671b3912955b1a49245cb66aa79eb2f
Matt Proctor <proctormatt0@gmail.com>
2023-12-31 18:25:35 -0800

    Updated login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,6 +8,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { type GetServerSideProps } from "next";
 import { getSession, signIn, useSession } from "next-auth/react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { z } from "zod";
@@ -60,87 +61,42 @@ const Login = () => {
 
   if (status === "unauthenticated") {
     return (
-      <Page title="Login" style="centered">
-        {/* <div
-          className="absolute top-0 z-0 h-screen w-full"
-          style={{
-            backgroundImage: `url(/static/images/stacked-waves-horizontal-light.svg)`,
-            backgroundSize: "cover",
-          }}
-        ></div> */}
-        <div className="absolute top-0 flex h-screen w-full items-center justify-center p-4">
-          <div className="w-full max-w-sm">
-            <div className="items-center">
-              <div className="flex flex-col justify-center gap-2 rounded-xl px-3 py-1 text-left text-5xl font-bold text-primary-light">
-                <span>Welcome to</span>
-                <div className="flex items-center gap-2">
-                  <FontAwesomeIcon
-                    className="h-10 w-10 text-primary-light"
-                    icon={faCircleHalfStroke}
-                  />
-                  <span>Dink</span>
-                </div>
-              </div>
-              <div className="mt-10 flex w-full max-w-sm flex-col gap-4 rounded-xl bg-gray-100 p-4 shadow-md">
-                <Button
-                  title="Try the Demo"
-                  className="w-full"
-                  style={open ? "primary" : "secondary"}
-                  icon={faPaperPlane}
-                  onClick={() => void signIn("google")}
+      <>
+        <Head>
+          <title>Home</title>
+        </Head>
+        <main className="flex h-screen w-full items-end justify-center">
+          <div
+            className="absolute top-0 z-0 h-screen w-full"
+            style={{
+              backgroundImage: `url(/static/images/stacked-waves-horizontal-light.svg)`,
+              backgroundSize: "cover",
+            }}
+          ></div>
+          <div className="z-10 flex h-3/5 w-full max-w-xs flex-col justify-between pt-8 pb-20 lg:pt-0">
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center justify-center gap-2 text-primary-light">
+                <FontAwesomeIcon
+                  size="4x"
+                  className=""
+                  icon={faCircleHalfStroke}
                 />
-                <div className="flex items-center justify-center gap-3 py-2">
-                  <div className="h-0.5 w-full rounded-full bg-gray-300" />
-                  <span className="text-xs font-bold text-gray-300">OR</span>
-                  <div className="h-0.5 w-full rounded-full bg-gray-300" />
-                </div>
-                {!open && (
-                  <button
-                    className="flex w-full items-center justify-center gap-2 rounded-xl border border-gray-300 bg-white px-4 py-2 font-bold text-gray-600 shadow-md"
-                    onClick={() => setOpen(true)}
-                  >
-                    <FontAwesomeIcon icon={faEnvelope} />
-                    Request Full Access
-                  </button>
-                )}
-                {open && (
-                  <form
-                    onSubmit={requestAccess}
-                    className="flex w-full flex-col gap-4"
-                  >
-                    <div className="flex items-center gap-2">
-                      <input
-                        autoFocus
-                        placeholder="Enter Email..."
-                        className="w-full rounded-xl bg-primary-med py-2 px-5 text-lg font-bold text-primary-light placeholder-primary-light"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                      />
-                      <IconButton
-                        size="sm"
-                        icon={faSquareXmark}
-                        onClick={() => {
-                          setOpen(false);
-                          setEmail("");
-                        }}
-                      />
-                    </div>
-                    <Button
-                      title={open ? "Submit" : "Request Access"}
-                      className="w-full"
-                      style="secondary"
-                      size="lg"
-                      icon={open ? faArrowRight : faEnvelope}
-                      type="submit"
-                      iconRight={open}
-                    />
-                  </form>
-                )}
+                <h1 className="text-7xl font-extrabold">Dink</h1>
               </div>
+              <h3 className="text-center font-bold text-white">
+                Simplify the way you save
+              </h3>
             </div>
+            <button
+              className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 px-4 py-2 text-lg font-bold text-white shadow-md shadow-primary-dark"
+              onClick={() => void signIn("google")}
+            >
+              Get started
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
           </div>
-        </div>
-      </Page>
+        </main>
+      </>
     );
   }
   return <LoadingPage />;
