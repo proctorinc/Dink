dbf48b6e7804602069496c60142c3a5db54328d1
Matt Proctor <proctormatt0@gmail.com>
2024-06-04 10:46:44 -0700

    Updated database url naming

--- src/env.mjs
+++ src/env.mjs
@@ -42,7 +42,7 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL: process.env.POSTGRES_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
